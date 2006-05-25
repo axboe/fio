2f844d305f4b3cda8af0e53dc8873cfd899c5855
Jens Axboe <axboe@suse.de>
2006-05-25 23:01:01 +0200

    [PATCH] Only fsync on close if fsync= given

--- fio.c
+++ fio.c
@@ -1050,7 +1050,7 @@ static void do_io(struct thread_data *td)
 	if (td->cur_depth)
 		cleanup_pending_aio(td);
 
-	if (should_fsync(td))
+	if (should_fsync(td) && td->fsync_blocks)
 		sync_td(td);
 }
 
