aae22ca71e8f5ac94a79f5dee161533a25852f6c
Jens Axboe <axboe@kernel.dk>
2006-09-05 10:46:22 +0200

    [PATCH] email update

--- fio.c
+++ fio.c
@@ -2,6 +2,7 @@
  * fio - the flexible io tester
  *
  * Copyright (C) 2005 Jens Axboe <axboe@suse.de>
+ * Copyright (C) 2006 Jens Axboe <axboe@kernel.dk>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
