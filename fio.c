8e9fe637622aab409101c90d26670375a50ff4f3
Jens Axboe <jens.axboe@oracle.com>
2006-10-24 15:11:09 +0200

    [PATCH] License update
    We target GPL v2 exclusively. Also note that this covers all files
    distributed with fio, not just fio.c itself.
    
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- fio.c
+++ fio.c
@@ -4,10 +4,12 @@
  * Copyright (C) 2005 Jens Axboe <axboe@suse.de>
  * Copyright (C) 2006 Jens Axboe <axboe@kernel.dk>
  *
+ * The license below covers all files distributed with fio unless otherwise
+ * noted in the file itself.
+ *
  *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
