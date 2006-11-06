c3054f90aa0d7f258eccbdd91c21c9fd8f81736a
Jens Axboe <jens.axboe@oracle.com>
2006-11-06 14:21:49 +0100

    [PATCH] Version 1.8
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- init.c
+++ init.c
@@ -409,7 +409,7 @@ static struct option long_options[FIO_JOB_OPTS + FIO_CMD_OPTS] = {
 
 static int def_timeout = DEF_TIMEOUT;
 
-static char fio_version_string[] = "fio 1.7";
+static char fio_version_string[] = "fio 1.8";
 
 static char **ini_file;
 static int max_jobs = MAX_JOBS;
