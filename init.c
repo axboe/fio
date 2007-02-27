e77fe33a06fc0aaccb5d14fe6d257b6313cbf2ef
Jens Axboe <jens.axboe@oracle.com>
2007-02-27 20:38:51 +0100

    Fio version 1.12
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- init.c
+++ init.c
@@ -590,7 +590,7 @@ static struct option long_options[FIO_JOB_OPTS + FIO_CMD_OPTS] = {
 
 static int def_timeout = 0;
 
-static char fio_version_string[] = "fio 1.11";
+static char fio_version_string[] = "fio 1.12";
 
 static char **ini_file;
 static int max_jobs = MAX_JOBS;
