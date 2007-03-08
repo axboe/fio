axboe/fio

commit b5af82930ccfd7dda6a1b11794efb452eb76d8dc

Revamp file open/close handling
Some IO engines need special handling for opening and closing
files, and this has complicated the fio filesetup.c file. Instead
have the io engine provide hooks for file open/close. This also
greatly cleans up the flags (we can get rid of SELFOPEN and MMAPIO)
and moves private knowledge into the engines where it belongs.

This potentially destabilizes fio somewhat, so testing is needed.
The new openfiles option that is introduced with this change isn't
verified working yet, hence it isn't documented.

Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

io_u.c
@@ -107,7 +107,7 @@ static int get_next_offset(struct thread_data *td, struct io_u *io_u)
 
 	if (td_random(td)) {
 		unsigned long long max_blocks = f->file_size / td->min_bs[ddir];
-		int loops = 5;
+		int loops = 2;
 
 		do {
 			r = os_random_long(&td->random_state);
@@ -335,7 +335,7 @@ static struct fio_file *get_next_file_rand(struct thread_data *td)
 	do {
 		long r = os_random_long(&td->next_file_state);
 
-		fileno = (unsigned int) ((double) (td->nr_files - 1) * r / (RAND_MAX + 1.0));
+		fileno = (unsigned int) ((double) (td->open_files - 1) * r / (RAND_MAX + 1.0));
 		f = &td->files[fileno];
 		if (f->fd != -1)
 			return f;
@@ -354,7 +354,7 @@ static struct fio_file *get_next_file_rr(struct thread_data *td)
 		f = &td->files[td->next_file];
 
 		td->next_file++;
-		if (td->next_file >= td->nr_files)
+		if (td->next_file >= td->open_files)
 			td->next_file = 0;
 
 		if (f->fd != -1)
@@ -441,7 +441,15 @@ struct io_u *get_io_u(struct thread_data *td)
 		 * No more to do for this file, close it
 		 */
 		io_u->file = NULL;
-		close_file(td, f);
+		td_io_close_file(td, f);
+
+		/*
+		 * probably not the right place to do this, but see
+		 * if we need to open a new file
+		 */
+		if (td->nr_open_files < td->nr_files &&
+		    td->open_files != td->nr_files)
+			reopen_file(td, f);
 	} while (1);
 
 	if (td->zone_bytes >= td->zone_size) {

engines/libaio.c
@@ -219,6 +219,8 @@ static struct ioengine_ops ioengine = {
 	.getevents	= fio_libaio_getevents,
 	.event		= fio_libaio_event,
 	.cleanup	= fio_libaio_cleanup,
+	.open_file	= generic_open_file,
+	.close_file	= generic_close_file,
 };
 
 #else /* FIO_HAVE_LIBAIO */

engines/mmap.c
@@ -64,12 +64,72 @@ static int fio_mmapio_init(struct thread_data *td)
 	return 0;
 }
 
+static int fio_mmapio_open(struct thread_data *td, struct fio_file *f)
+{
+	int ret, flags;
+
+	ret = generic_open_file(td, f);
+	if (ret)
+		return ret;
+
+	if (td_rw(td))
+		flags = PROT_READ | PROT_WRITE;
+	else if (td_write(td)) {
+		flags = PROT_WRITE;
+
+		if (td->verify != VERIFY_NONE)
+			flags |= PROT_READ;
+	} else
+		flags = PROT_READ;
+
+	f->mmap = mmap(NULL, f->file_size, flags, MAP_SHARED, f->fd, f->file_offset);
+	if (f->mmap == MAP_FAILED) {
+		f->mmap = NULL;
+		td_verror(td, errno, "mmap");
+		goto err;
+	}
+
+	if (file_invalidate_cache(td, f))
+		goto err;
+
+	if (!td_random(td)) {
+		if (madvise(f->mmap, f->file_size, MADV_SEQUENTIAL) < 0) {
+			td_verror(td, errno, "madvise");
+			goto err;
+		}
+	} else {
+		if (madvise(f->mmap, f->file_size, MADV_RANDOM) < 0) {
+			td_verror(td, errno, "madvise");
+			goto err;
+		}
+	}
+
+	return 0;
+
+err:
+	if (f->mmap)
+		munmap(f->mmap, f->file_size);
+	generic_close_file(td, f);
+	return 1;
+}
+
+static void fio_mmapio_close(struct thread_data fio_unused *td,
+			     struct fio_file *f)
+{
+	if (f->mmap) {
+		munmap(f->mmap, f->file_size);
+		f->mmap = NULL;
+	}
+}
+
 static struct ioengine_ops ioengine = {
 	.name		= "mmap",
 	.version	= FIO_IOOPS_VERSION,
 	.queue		= fio_mmapio_queue,
 	.init		= fio_mmapio_init,
-	.flags		= FIO_SYNCIO | FIO_MMAPIO,
+	.open_file	= fio_mmapio_open,
+	.close_file	= fio_mmapio_close,
+	.flags		= FIO_SYNCIO,
 };
 
 static void fio_init fio_mmapio_register(void)

engines/net.c
@@ -14,17 +14,23 @@
 #include "../fio.h"
 #include "../os.h"
 
-#define send_to_net(td)	((td)->io_ops->priv)
+struct netio_data {
+	int listenfd;
+	int send_to_net;
+	char host[64];
+	struct sockaddr_in addr;
+};
 
 static int fio_netio_prep(struct thread_data *td, struct io_u *io_u)
 {
+	struct netio_data *nd = td->io_ops->data;
 	struct fio_file *f = io_u->file;
 
 	/*
 	 * Make sure we don't see spurious reads to a receiver, and vice versa
 	 */
-	if ((send_to_net(td) && io_u->ddir == DDIR_READ) ||
-	    (!send_to_net(td) && io_u->ddir == DDIR_WRITE)) {
+	if ((nd->send_to_net && io_u->ddir == DDIR_READ) ||
+	    (!nd->send_to_net && io_u->ddir == DDIR_WRITE)) {
 		td_verror(td, EINVAL, "bad direction");
 		return 1;
 	}
@@ -77,64 +83,39 @@ static int fio_netio_queue(struct thread_data *td, struct io_u *io_u)
 	return FIO_Q_COMPLETED;
 }
 
-static int fio_netio_setup_connect(struct thread_data *td, const char *host,
-				   unsigned short port)
+static int fio_netio_connect(struct thread_data *td, struct fio_file *f)
 {
-	struct sockaddr_in addr;
-	struct fio_file *f;
-	int i;
+	struct netio_data *nd = td->io_ops->data;
 
-	memset(&addr, 0, sizeof(addr));
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(port);
-
-	if (inet_aton(host, &addr.sin_addr) != 1) {
-		struct hostent *hent;
-
-		hent = gethostbyname(host);
-		if (!hent) {
-			td_verror(td, errno, "gethostbyname");
-			return 1;
-		}
-
-		memcpy(&addr.sin_addr, hent->h_addr, 4);
+	f->fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (f->fd < 0) {
+		td_verror(td, errno, "socket");
+		return 1;
 	}
 
-	for_each_file(td, f, i) {
-		f->fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-		if (f->fd < 0) {
-			td_verror(td, errno, "socket");
-			return 1;
-		}
-
-		if (connect(f->fd, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
-			td_verror(td, errno, "connect");
-			return 1;
-		}
+	if (connect(f->fd, (struct sockaddr *) &nd->addr, sizeof(nd->addr)) < 0) {
+		td_verror(td, errno, "connect");
+		return 1;
 	}
 
 	return 0;
-
 }
 
-static int fio_netio_accept_connections(struct thread_data *td, int fd,
-					struct sockaddr_in *addr)
+static int fio_netio_accept(struct thread_data *td, struct fio_file *f)
 {
-	socklen_t socklen = sizeof(*addr);
-	unsigned int accepts = 0;
+	struct netio_data *nd = td->io_ops->data;
+	socklen_t socklen = sizeof(nd->addr);
 	struct pollfd pfd;
+	int ret;
 
-	fprintf(f_out, "fio: waiting for %u connections\n", td->nr_files);
+	fprintf(f_out, "fio: waiting for connection\n");
 
 	/*
 	 * Accept loop. poll for incoming events, accept them. Repeat until we
 	 * have all connections.
 	 */
-	while (!td->terminate && accepts < td->nr_files) {
-		struct fio_file *f;
-		int ret, i;
-
-		pfd.fd = fd;
+	while (!td->terminate) {
+		pfd.fd = nd->listenfd;
 		pfd.events = POLLIN;
 
 		ret = poll(&pfd, 1, -1);
@@ -153,26 +134,52 @@ static int fio_netio_accept_connections(struct thread_data *td, int fd,
 		if (!(pfd.revents & POLLIN))
 			continue;
 
-		for_each_file(td, f, i) {
-			if (f->fd != -1)
-				continue;
-
-			f->fd = accept(fd, (struct sockaddr *) addr, &socklen);
-			if (f->fd < 0) {
-				td_verror(td, errno, "accept");
-				return 1;
-			}
-			accepts++;
-			break;
+		f->fd = accept(nd->listenfd, (struct sockaddr *) &nd->addr, &socklen);
+		if (f->fd < 0) {
+			td_verror(td, errno, "accept");
+			return 1;
 		}
+		break;
 	}
 
 	return 0;
 }
 
-static int fio_netio_setup_listen(struct thread_data *td, unsigned short port)
+
+static int fio_netio_open_file(struct thread_data *td, struct fio_file *f)
 {
-	struct sockaddr_in addr;
+	if (td_read(td))
+		return fio_netio_accept(td, f);
+	else
+		return fio_netio_connect(td, f);
+}
+
+static int fio_netio_setup_connect(struct thread_data *td, const char *host,
+				   unsigned short port)
+{
+	struct netio_data *nd = td->io_ops->data;
+
+	nd->addr.sin_family = AF_INET;
+	nd->addr.sin_port = htons(port);
+
+	if (inet_aton(host, &nd->addr.sin_addr) != 1) {
+		struct hostent *hent;
+
+		hent = gethostbyname(host);
+		if (!hent) {
+			td_verror(td, errno, "gethostbyname");
+			return 1;
+		}
+
+		memcpy(&nd->addr.sin_addr, hent->h_addr, 4);
+	}
+
+	return 0;
+}
+
+static int fio_netio_setup_listen(struct thread_data *td, short port)
+{
+	struct netio_data *nd = td->io_ops->data;
 	int fd, opt;
 
 	fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -193,12 +200,11 @@ static int fio_netio_setup_listen(struct thread_data *td, unsigned short port)
 	}
 #endif
 
-	memset(&addr, 0, sizeof(addr));
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	addr.sin_port = htons(port);
+	nd->addr.sin_family = AF_INET;
+	nd->addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	nd->addr.sin_port = htons(port);
 
-	if (bind(fd, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
+	if (bind(fd, (struct sockaddr *) &nd->addr, sizeof(nd->addr)) < 0) {
 		td_verror(td, errno, "bind");
 		return 1;
 	}
@@ -207,14 +213,16 @@ static int fio_netio_setup_listen(struct thread_data *td, unsigned short port)
 		return 1;
 	}
 
-	return fio_netio_accept_connections(td, fd, &addr);
+	nd->listenfd = fd;
+	return 0;
 }
 
 static int fio_netio_init(struct thread_data *td)
 {
-	char host[64], buf[128];
+	struct netio_data *nd = td->io_ops->data;
 	unsigned short port;
 	struct fio_file *f;
+	char host[64], buf[128];
 	char *sep;
 	int ret, i;
 
@@ -242,10 +250,10 @@ static int fio_netio_init(struct thread_data *td)
 	port = atoi(sep);
 
 	if (td_read(td)) {
-		send_to_net(td) = 0;
+		nd->send_to_net = 0;
 		ret = fio_netio_setup_listen(td, port);
 	} else {
-		send_to_net(td) = 1;
+		nd->send_to_net = 1;
 		ret = fio_netio_setup_connect(td, host, port);
 	}
 
@@ -264,8 +272,23 @@ static int fio_netio_init(struct thread_data *td)
 	return 0;
 }
 
-static int fio_netio_setup(struct thread_data fio_unused *td)
+static void fio_netio_cleanup(struct thread_data *td)
 {
+	struct netio_data *nd = td->io_ops->data;
+
+	if (nd) {
+		free(nd);
+		td->io_ops->data = NULL;
+	}
+}
+
+static int fio_netio_setup(struct thread_data *td)
+{
+	struct netio_data *nd = malloc(sizeof(*nd));
+
+	memset(nd, 0, sizeof(*nd));
+	nd->listenfd = -1;
+	td->io_ops->data = nd;
 	return 0;
 }
 
@@ -276,7 +299,10 @@ static struct ioengine_ops ioengine = {
 	.queue		= fio_netio_queue,
 	.setup		= fio_netio_setup,
 	.init		= fio_netio_init,
-	.flags		= FIO_SYNCIO | FIO_DISKLESSIO | FIO_SELFOPEN,
+	.cleanup	= fio_netio_cleanup,
+	.open_file	= fio_netio_open_file,
+	.close_file	= generic_close_file,
+	.flags		= FIO_SYNCIO | FIO_DISKLESSIO,
 };
 
 static void fio_init fio_netio_register(void)

engines/null.c
@@ -41,12 +41,19 @@ static int fio_null_setup(struct thread_data *td)
 	return 0;
 }
 
+static int fio_null_open(struct thread_data fio_unused *td,
+			 struct fio_file fio_unused *f)
+{
+	return 0;
+}
+
 static struct ioengine_ops ioengine = {
 	.name		= "null",
 	.version	= FIO_IOOPS_VERSION,
 	.setup		= fio_null_setup,
 	.queue		= fio_null_queue,
-	.flags		= FIO_SYNCIO | FIO_DISKLESSIO | FIO_SELFOPEN,
+	.open_file	= fio_null_open,
+	.flags		= FIO_SYNCIO | FIO_DISKLESSIO,
 };
 
 static void fio_init fio_null_register(void)

engines/posixaio.c
@@ -194,6 +194,8 @@ static struct ioengine_ops ioengine = {
 	.getevents	= fio_posixaio_getevents,
 	.event		= fio_posixaio_event,
 	.cleanup	= fio_posixaio_cleanup,
+	.open_file	= generic_open_file,
+	.close_file	= generic_close_file,
 };
 
 #else /* FIO_HAVE_POSIXAIO */

engines/sg.c
@@ -26,6 +26,7 @@ struct sgio_data {
 	int *fd_flags;
 	void *sgbuf;
 	unsigned int bs;
+	int type_checked;
 };
 
 static void sgio_hdr_init(struct sgio_data *sd, struct sg_io_hdr *hdr,
@@ -317,10 +318,7 @@ static void fio_sgio_cleanup(struct thread_data *td)
 
 static int fio_sgio_init(struct thread_data *td)
 {
-	struct fio_file *f = &td->files[0];
 	struct sgio_data *sd;
-	unsigned int bs;
-	int ret;
 
 	sd = malloc(sizeof(*sd));
 	memset(sd, 0, sizeof(*sd));
@@ -337,25 +335,37 @@ static int fio_sgio_init(struct thread_data *td)
 
 	td->io_ops->data = sd;
 
+	/*
+	 * we want to do it, regardless of whether odirect is set or not
+	 */
+	td->override_sync = 1;
+	return 0;
+}
+
+static int fio_sgio_type_check(struct thread_data *td, struct fio_file *f)
+{
+	struct sgio_data *sd = td->io_ops->data;
+	unsigned int bs;
+
 	if (td->filetype == FIO_TYPE_BD) {
 		if (ioctl(f->fd, BLKSSZGET, &bs) < 0) {
 			td_verror(td, errno, "ioctl");
-			goto err;
+			return 1;
 		}
 	} else if (td->filetype == FIO_TYPE_CHAR) {
-		int version;
+		int version, ret;
 
 		if (ioctl(f->fd, SG_GET_VERSION_NUM, &version) < 0) {
 			td_verror(td, errno, "ioctl");
-			goto err;
+			return 1;
 		}
 
 		ret = fio_sgio_get_bs(td, &bs);
 		if (ret)
-			goto err;
+			return 1;
 	} else {
 		log_err("ioengine sgio only works on block devices\n");
-		goto err;
+		return 1;
 	}
 
 	sd->bs = bs;
@@ -365,20 +375,24 @@ static int fio_sgio_init(struct thread_data *td)
 		td->io_ops->event = NULL;
 	}
 
-	/*
-	 * we want to do it, regardless of whether odirect is set or not
-	 */
-	td->override_sync = 1;
 	return 0;
-err:
-	free(sd->events);
-	free(sd->cmds);
-	free(sd->fd_flags);
-	free(sd->pfds);
-	free(sd->sgbuf);
-	free(sd);
-	td->io_ops->data = NULL;
-	return 1;
+}
+
+static int fio_sgio_open(struct thread_data *td, struct fio_file *f)
+{
+	struct sgio_data *sd = td->io_ops->data;
+	int ret;
+
+	ret = generic_open_file(td, f);
+	if (ret)
+		return ret;
+
+	if (!sd->type_checked && fio_sgio_type_check(td, f)) {
+		generic_close_file(td, f);
+		return 1;
+	}
+
+	return 0;
 }
 
 static struct ioengine_ops ioengine = {
@@ -390,6 +404,8 @@ static struct ioengine_ops ioengine = {
 	.getevents	= fio_sgio_getevents,
 	.event		= fio_sgio_event,
 	.cleanup	= fio_sgio_cleanup,
+	.open_file	= fio_sgio_open,
+	.close_file	= generic_close_file,
 	.flags		= FIO_SYNCIO | FIO_RAWIO,
 };
 

engines/skeleton_external.c
@@ -104,6 +104,23 @@ static void fio_skeleton_cleanup(struct thread_data *td)
 }
 
 /*
+ * Hook for opening the given file. Unless the engine has special
+ * needs, it usually just provides generic_file_open() as the handler.
+ */
+static int fio_skeleton_open(struct thread_data *td, struct fio_file *f)
+{
+	return generic_file_open(td, f);
+}
+
+/*
+ * Hook for closing a file. See fio_skeleton_open().
+ */
+static int fio_skeleton_close(struct thread_data *td, struct fio_file *f)
+{
+	generic_file_close(td, f);
+}
+
+/*
  * Note that the structure is exported, so that fio can get it via
  * dlsym(..., "ioengine");
  */
@@ -117,4 +134,6 @@ struct ioengine_ops ioengine = {
 	.getevents	= fio_skeleton_getevents,
 	.event		= fio_skeleton_event,
 	.cleanup	= fio_skeleton_cleanup,
+	.open_file	= fio_skeleton_open,
+	.close_file	= fio_skeleton_close,
 };

commit e6433576b85eb28d32fe099b38ea64ac006b4b74

get_next_offset() number of loops change
Commit b5af82930ccfd7dda6a1b11794efb452eb76d8dc inadvertently
changed this, revert.

Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

io_u.c
@@ -107,7 +107,7 @@ static int get_next_offset(struct thread_data *td, struct io_u *io_u)
 
 	if (td_random(td)) {
 		unsigned long long max_blocks = f->file_size / td->min_bs[ddir];
-		int loops = 2;
+		int loops = 5;
 
 		do {
 			r = os_random_long(&td->random_state);