f6a72c738dc58be7718aac2c57fb43f193ae056f
Jens Axboe <jens.axboe@oracle.com>
2008-03-07 14:28:38 +0100

    Fio 1.20-rc3
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- init.c
+++ init.c
@@ -19,7 +19,7 @@
 #include "smalloc.h"
 #include "filehash.h"
 
-static char fio_version_string[] = "fio 1.20-rc2";
+static char fio_version_string[] = "fio 1.20-rc3";
 
 #define FIO_RANDSEED		(0xb1899bedUL)
 
