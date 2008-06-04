9a9c63f1608c6f63b10cd5575dee868c2d269c87
Jens Axboe <jens.axboe@oracle.com>
2008-06-04 14:48:29 +0200

    Add missing splice syscall numbers for alpha and s390
    All archs now verified as compiling.
    
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- arch/arch-alpha.h
+++ arch/arch-alpha.h
@@ -12,6 +12,12 @@
 #define __NR_fadvise64		413
 #endif
 
+#ifndef __NR_sys_splice
+#define __NR_sys_splice		468
+#define __NR_sys_tee		470
+#define __NR_sys_vmsplice	471
+#endif
+
 #define nop			do { } while (0)
 #define read_barrier()		__asm__ __volatile__("mb": : :"memory")
 #define writer_barrier()	__asm__ __volatile__("wmb": : :"memory")
--- arch/arch-s390.h
+++ arch/arch-s390.h
@@ -12,6 +12,12 @@
 #define __NR_fadvise64		253
 #endif
 
+#ifndef __NR_sys_splice
+#define __NR_sys_splice		306
+#define __NR_sys_tee		308
+#define __NR_sys_vmsplice	309
+#endif
+
 #define nop		asm volatile ("diag 0,0,68" : : : "memory")
 #define read_barrier()	asm volatile("bcr 15,0" : : : "memory")
 #define write_barrier()	asm volatile("bcr 15,0" : : : "memory")
