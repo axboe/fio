a7ba8c5f4c0f9fe117d529c1ad6686b2070bae51
Shaozhi Shawn Ye <yeshao@google.com>
2008-09-10 09:04:18 +0200

    filesetup: When terse_output is set, The 'laying out' message should not be displayed.
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- filesetup.c
+++ filesetup.c
@@ -543,8 +543,10 @@ int setup_files(struct thread_data *td)
 	 */
 	if (need_extend) {
 		temp_stall_ts = 1;
-		log_info("%s: Laying out IO file(s) (%u file(s) / %LuMiB)\n",
-			td->o.name, need_extend, extend_size >> 20);
+		if (!terse_output)
+			log_info("%s: Laying out IO file(s) (%u file(s) /"
+				 " %LuMiB)\n", td->o.name, need_extend,
+					extend_size >> 20);
 
 		for_each_file(td, f, i) {
 			unsigned long long old_len, extend_len;
