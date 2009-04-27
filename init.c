97c62e0fd052854d9a8fb68cd4d0ed56bf59bc25
Jens Axboe <jens.axboe@oracle.com>
2009-04-27 08:05:45 +0200

    Fio 1.26.1
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- init.c
+++ init.c
@@ -19,7 +19,7 @@
 #include "smalloc.h"
 #include "filehash.h"
 
-static char fio_version_string[] = "fio 1.26";
+static char fio_version_string[] = "fio 1.26.1";
 
 #define FIO_RANDSEED		(0xb1899bedUL)
 
