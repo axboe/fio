55093ac92265a5132a67d2207aff7e0459086c22
Jens Axboe <jens.axboe@oracle.com>
2009-05-20 11:53:04 +0200

    Fio 1.26.4
    pre_read option was buggy, so tag a new release now.
    
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- init.c
+++ init.c
@@ -19,7 +19,7 @@
 #include "smalloc.h"
 #include "filehash.h"
 
-static char fio_version_string[] = "fio 1.26.3";
+static char fio_version_string[] = "fio 1.26.4";
 
 #define FIO_RANDSEED		(0xb1899bedUL)
 
