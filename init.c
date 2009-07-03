3d87c7a89fe27e2c593ab690a56911375aff99b5
Jens Axboe <jens.axboe@oracle.com>
2009-07-03 08:40:55 +0200

    Fio 1.30
    O_DIRECT accidentically got broken by the iomem_align option, so
    lets just cut a new major release.
    
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- init.c
+++ init.c
@@ -20,7 +20,7 @@
 #include "filehash.h"
 #include "verify.h"
 
-static char fio_version_string[] = "fio 1.29";
+static char fio_version_string[] = "fio 1.30";
 
 #define FIO_RANDSEED		(0xb1899bedUL)
 
