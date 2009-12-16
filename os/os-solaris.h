d0983d0124fff305d0945098fe7b4b9c44657fd8
Jens Axboe <jens.axboe@oracle.com>
2009-12-16 09:53:24 +0100

    Add Solaris helper object
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- os/os-solaris.h
+++ os/os-solaris.h
@@ -103,17 +103,4 @@ static inline int fio_cpuset_exit(os_cpu_mask_t *mask)
 #define FIO_MADV_FREE	MADV_FREE
 #endif
 
-/*
- * Some Solaris versions don't have posix_memalign(), provide a private
- * weak alternative
- */
-static inline int __weak posix_memalign(void **ptr, size_t align, size_t size)
-{
-	*ptr = memalign(align, size);
-	if (*ptr)
-		return 0;
-
-	return ENOMEM;
-}
-
 #endif
