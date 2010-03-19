2d5c40e77a4ee9103de9a43f016d77858366e1a2
Jens Axboe <jens.axboe@oracle.com>
2010-03-19 19:09:37 +0100

    __dprint() fixup
    Only define __dprint() function when FIO_INC_DEBUG is set. And
    don't check debug type once, not both in dprint() and __dprint().
    
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- debug.c
+++ debug.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include "debug.h"
 
+#ifdef FIO_INC_DEBUG
 void __dprint(int type, const char *str, ...)
 {
 	va_list args;
@@ -10,18 +11,16 @@ void __dprint(int type, const char *str, ...)
 
 	assert(type < FD_DEBUG_MAX);
 
-	if ((((1 << type)) & fio_debug) == 0)
-		return;
-
 	pid = getpid();
 	if (fio_debug_jobp && *fio_debug_jobp != -1U
 	    && pid != *fio_debug_jobp)
 		return;
 
-	log_info("%-8s ", debug_levels[(type)].name);
+	log_info("%-8s ", debug_levels[type].name);
 	log_info("%-5u ", (int) pid);
 
 	va_start(args, str);
 	log_valist(str, args);
 	va_end(args);
 }
+#endif
