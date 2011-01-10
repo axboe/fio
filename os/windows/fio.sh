8bad3c5b15c221cbd141a357d8ba81ca4cc45cac
Jens Axboe <jaxboe@fusionio.com>
2011-01-10 19:36:09 +0100

    Remove left-over files
    Signed-off-by: Jens Axboe <jaxboe@fusionio.com>

--- os/windows/fio.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-/usr/sbin/cygserver > /dev/null &
-/bin/real_fio $@
-/usr/sbin/cygserver -S > /dev/null
--- os/windows/fio/Cygwin.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-C:
-IF EXIST     "%PROGRAMFILES(X86)%" chdir "%PROGRAMFILES(X86)%\fio\bin"
-IF NOT EXIST "%PROGRAMFILES(X86)%" chdir "%PROGRAMFILES%\fio\bin"
-
-bash -c "echo \"FIO is available as /bin/fio - type fio to run it.\" && echo \"Examples are in /usr/share/doc/fio/examples\" && echo \"Type \"cd\" to change directory and dir (or ls) to see directory contents.\" && echo \"To exit, close the window.\" && /usr/sbin/cygserver & 2> /dev/null"
-bash --login -i
