6035e7d6d1ed462176402615da37d4f987588c16
Jens Axboe <jaxboe@fusionio.com>
2011-01-24 21:53:27 +0100

    Remove flist_sort(), it's no longer used
    Signed-off-by: Jens Axboe <jaxboe@fusionio.com>

--- flist_sort.h
+++
@@ -1,9 +0,0 @@
-#ifndef FIO_FLIST_SORT_H
-#define FIO_FLIST_SORT_H
-
-struct flist_head;
-
-void flist_sort(void *priv, struct flist_head *head,
-	       int (*cmp)(void *priv, struct flist_head *a,
-			  struct flist_head *b));
-#endif
