0dc1bc03182ff0c8d1f3b1c36ce8e2f5bba52af6
Jens Axboe <axboe@kernel.dk>
2011-10-13 08:55:29 +0200

    Disable clat percentiles if gtod_reduce=1 is set
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- options.c
+++ options.c
@@ -777,6 +777,7 @@ static int str_gtod_reduce_cb(void *data, int *il)
 	td->o.disable_clat = !!val;
 	td->o.disable_slat = !!val;
 	td->o.disable_bw = !!val;
+	td->o.clat_percentiles = !val;
 	if (val)
 		td->tv_cache_mask = 63;
 
