b77595688b752c40cc50a33471aa0e4de08da0c6
Stephen M. Cameron <stephenmcameron@gmail.com>
2012-02-24 08:17:31 +0100

    fio: factor out endian_check
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

---
+++ endian_check.h
@@ -0,0 +1,6 @@
+#ifndef ENDIAN_CHECK_H
+#define ENDIAN_CHECK_H
+
+int endian_check(void);
+
+#endif 
--- fio.c
+++ fio.c
@@ -36,39 +36,10 @@
 #include "memalign.h"
 #include "client.h"
 #include "server.h"
+#include "endian_check.h"
 
 unsigned long page_mask;
 unsigned long page_size;
-
-static int endian_check(void)
-{
-	union {
-		uint8_t c[8];
-		uint64_t v;
-	} u;
-	int le = 0, be = 0;
-
-	u.v = 0x12;
-	if (u.c[7] == 0x12)
-		be = 1;
-	else if (u.c[0] == 0x12)
-		le = 1;
-
-#if defined(FIO_LITTLE_ENDIAN)
-	if (be)
-		return 1;
-#elif defined(FIO_BIG_ENDIAN)
-	if (le)
-		return 1;
-#else
-	return 1;
-#endif
-
-	if (!le && !be)
-		return 1;
-
-	return 0;
-}
 
 int main(int argc, char *argv[], char *envp[])
 {
