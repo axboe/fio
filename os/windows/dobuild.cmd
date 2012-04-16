84306c1d9ecd94e93436da62b41ef61ac6e281ac
Bruce Cran <bruce@cran.org.uk>
2012-04-16 19:33:03 +0200

    Windows build fixes
    Bruce writes:
    
    SIGUSR1 isn't defined on Windows so I've defined it.
    
    Also, MSI packages need a version string of x.x.x.x where x is between 0
    and 65535 so we can't include the git hash. It's easier just to go back
    to hard-coding the version for now, and I'll update it (and the product
    GUID) when I create a new package (it doesn't need checked in each
    time).
    
    Also, Cygwin doesn't like $(SHELL_PATH) but $(SHELL) seems to work -
    does $(SHELL) cause a problem on other platforms?
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- os/windows/dobuild.cmd
+++ os/windows/dobuild.cmd
@@ -1,11 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
-set /a counter=4
-for /f "tokens=3" %%i in (..\..\fio_version.h) do (
- if "!counter!"=="4" set FIO_MAJOR=%%i
- if "!counter!"=="5" set FIO_MINOR=%%i
- if "!counter!"=="6" set FIO_PATCH=%%i
-set /a counter+=1
+set /a counter=1
+for /f "tokens=3" %%i in (..\..\FIO-VERSION-FILE) do (
+ if "!counter!"=="1" set FIO_VERSION=%%i
+ set /a counter+=1
 )
 
 if "%1"=="x86" set FIO_ARCH=x86
@@ -22,5 +20,5 @@ if not defined FIO_ARCH (
 @if ERRORLEVEL 1 goto end
 "%WIX%bin\candle" -nologo -arch %FIO_ARCH% examples.wxs
 @if ERRORLEVEL 1 goto end
-"%WIX%bin\light" -nologo install.wixobj examples.wixobj -ext WixUIExtension -out fio-%FIO_MAJOR%.%FIO_MINOR%.%FIO_PATCH%-%FIO_ARCH%.msi
+"%WIX%bin\light" -nologo install.wixobj examples.wixobj -ext WixUIExtension -out %FIO_VERSION%-%FIO_ARCH%.msi
 :end
