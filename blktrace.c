adfc73488616bac66f910d53c75202d5be8afe87
Jens Axboe <axboe@kernel.dk>
2012-06-11 08:58:42 +0200

    blktrace: use for_each_file() instead of open coding the loop
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- blktrace.c
+++ blktrace.c
@@ -418,10 +418,8 @@ int load_blktrace(struct thread_data *td, const char *filename)
 		}
 	} while (1);
 
-	for (i = 0; i < td->files_index; i++) {
-		f= td->files[i];
+	for_each_file(td, f, i)
 		trace_add_open_close_event(td, f->fileno, FIO_LOG_CLOSE_FILE);
-	}
 
 	fifo_free(fifo);
 	close(fd);
