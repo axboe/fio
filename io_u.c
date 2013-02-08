axboe/fio

commit e0224c6bd115b0decd4f330fa8ff43f5e0cc6811

Fix bug with rate and read/write mixed workloads at 100% bias
The rate_ddir() might sometimes decide to send the other data
direction, even if we have a workload with rw=randrw and
rwmixread=0. Check for this condition.

Signed-off-by: Jens Axboe <axboe@kernel.dk>

io_u.c
@@ -490,7 +490,7 @@ static enum fio_ddir rate_ddir(struct thread_data *td, enum fio_ddir ddir)
 
 	assert(ddir_rw(ddir));
 
-	if (td->rate_pending_usleep[ddir] <= 0)
+	if (td->rate_pending_usleep[ddir] <= 0 || !td->o.rwmix[odir])
 		return ddir;
 
 	/*
@@ -545,6 +545,7 @@ static enum fio_ddir rate_ddir(struct thread_data *td, enum fio_ddir ddir)
 
 	if (ddir_trim(ddir))
 		return ddir;
+
 	return ddir;
 }
 

commit 315fcfec5f22d6c0d059c4c3b70eaed53237eb5f

Fix rate limiting
Commit e0224c6b accidentally broke rate limiting, since
it bypassed the sleep logic. Rework it slightly to not
skip that.

Signed-off-by: Jens Axboe <axboe@kernel.dk>

io_u.c
@@ -490,14 +490,14 @@ static enum fio_ddir rate_ddir(struct thread_data *td, enum fio_ddir ddir)
 
 	assert(ddir_rw(ddir));
 
-	if (td->rate_pending_usleep[ddir] <= 0 || !td->o.rwmix[odir])
+	if (td->rate_pending_usleep[ddir] <= 0)
 		return ddir;
 
 	/*
 	 * We have too much pending sleep in this direction. See if we
 	 * should switch.
 	 */
-	if (td_rw(td)) {
+	if (td_rw(td) && td->o.rwmix[odir]) {
 		/*
 		 * Other direction does not have too much pending, switch
 		 */