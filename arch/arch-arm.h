2d3b79c7747eb077b85152d0bb82304b3eff40a6
Otavio Salvador <otavio@ossystems.com.br>
2013-04-15 09:36:24 +0200

    ARM: Use generic assembly nop and barrier code for armv6
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- arch/arch-arm.h
+++ arch/arch-arm.h
@@ -18,7 +18,9 @@
 #define __NR_sys_vmsplice	343
 #endif
 
-#if defined (__ARM_ARCH_4__) || defined (__ARM_ARCH_4T__) || defined (__ARM_ARCH_5__) || defined (__ARM_ARCH_5T__) || defined (__ARM_ARCH_5TE__) || defined (__ARM_ARCH_5TEJ__)
+#if defined (__ARM_ARCH_4__) || defined (__ARM_ARCH_4T__) \
+	|| defined (__ARM_ARCH_5__) || defined (__ARM_ARCH_5T__) || defined (__ARM_ARCH_5TE__) || defined (__ARM_ARCH_5TEJ__) \
+	|| defined(__ARM_ARCH_6__)  || defined(__ARM_ARCH_6J__) || defined(__ARM_ARCH_6Z__) || defined(__ARM_ARCH_6ZK__)
 #define nop             __asm__ __volatile__("mov\tr0,r0\t@ nop\n\t")
 #define read_barrier()	__asm__ __volatile__ ("" : : : "memory")
 #define write_barrier()	__asm__ __volatile__ ("" : : : "memory")
