3899da22920c2ac1b30080f915189eb77c94cf8a
Bruce Cran <bruce@cran.org.uk>
2013-05-29 10:12:27 +0100

    posix_fadvise is now provided in helpers.c: remove it from the Windows posix.c
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- os/windows/posix.c
+++ os/windows/posix.c
@@ -662,11 +662,6 @@ int getrusage(int who, struct rusage *r_usage)
 	return 0;
 }
 
-int posix_fadvise(int fd, off_t offset, off_t len, int advice)
-{
-	return 0;
-}
-
 int posix_madvise(void *addr, size_t len, int advice)
 {
 	log_err("%s is not implemented\n", __func__);
