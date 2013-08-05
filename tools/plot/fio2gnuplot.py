axboe/fio

commit f9846c392d176582916c39b1925a436e79f23de1

fio2gnuplot: Don't truncate fio log files
The code was stopping at reading once the shorter stopped. But in the
case of a very big difference of performances between hosts, some could
be much longer/shorter than others.

This patch does insure that until _all_ input files got read complety we
do had fake data with a negative time.

This way, all the traces will be complete as per fio's output.

tools/plot/fio2gnuplot.py
@@ -99,11 +99,18 @@ def compute_temp_file(fio_data_file,disk_perf):
 	shall_break = False
 	while True:
 		current_line=[]
+		nb_empty_files=0
+		nb_files=len(files)
 		for file in files:
 			s=file.readline().replace(',',' ').split()
 			if not s:
+				nb_empty_files+=1
+				s="-1, 0, 0, 0'".replace(',',' ').split()
+
+			if (nb_empty_files == nb_files):
 				shall_break=True
 				break;
+
 			current_line.append(s);
 
 		if shall_break == True:

commit 4ff1f2d08b89a84223bff6310db7a18946c42ecd

fio2gnuplot: Fixing typo when inserting fake data
Commit f9846c392d176582916c39b1925a436e79f23de1 introduced fake data
when matched files doesn't have the exact same amount of traces. This
patch introduced a typo that prevented the int comparison to work
properly.

tools/plot/fio2gnuplot.py
@@ -162,7 +162,7 @@ def compute_temp_file(fio_data_file,disk_perf,gnuplot_output_dir):
 			s=file.readline().replace(',',' ').split()
 			if not s:
 				nb_empty_files+=1
-				s="-1, 0, 0, 0'".replace(',',' ').split()
+				s="-1, 0, 0, 0".replace(',',' ').split()
 
 			if (nb_empty_files == nb_files):
 				shall_break=True