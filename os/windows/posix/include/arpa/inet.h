47c7c1f526f1038abe7df350d02231220b3eecb9
Bruce Cran <bcran@fusionio.com>
2014-01-28 12:54:17 -0700

    Fix Windows headers for IPv6
    MinGW includes all the necessary IPv6 definitions, so include ws2tcpip.h
    and remove the custom definitions.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- os/windows/posix/include/arpa/inet.h
+++ os/windows/posix/include/arpa/inet.h
@@ -1,21 +1,14 @@
 #ifndef ARPA_INET_H
 #define ARPA_INET_H
 
-#include <winsock2.h>
+#include <ws2tcpip.h>
 #include <inttypes.h>
 
 typedef int socklen_t;
 typedef int in_addr_t;
 
-#define IP_MULTICAST_IF 2
-#define IP_MULTICAST_TTL 3
-#define IP_ADD_MEMBERSHIP 5
-
-struct ip_mreq
-{
-	struct in_addr imr_multiaddr;
-	struct in_addr imr_interface;
-};
+/* EAI_SYSTEM isn't used on Windows, so map it to EAI_FAIL */
+#define EAI_SYSTEM EAI_FAIL
 
 in_addr_t inet_network(const char *cp);
 
--- os/windows/posix/include/netinet/in.h
+++ os/windows/posix/include/netinet/in.h
@@ -4,18 +4,5 @@
 #include <inttypes.h>
 #include <sys/un.h>
 
-struct in6_addr
-{
-	uint8_t s6_addr[16];
-};
-
-struct sockaddr_in6
-{
-	sa_family_t		sin6_family;   /* AF_INET6 */
-	in_port_t		sin6_port;     /* Port number */
-	uint32_t		sin6_flowinfo; /* IPv6 traffic class and flow information */
-	struct in6_addr	sin6_addr;     /* IPv6 address */
-	uint32_t		sin6_scope_id; /* Set of interfaces for a scope */
-};
 
 #endif /* NETINET_IN_H */
