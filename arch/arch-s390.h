919e789d0999bf13ef1ae670c536d2aef3709894
Christian Ehrhardt <ehrhardt@linux.vnet.ibm.com>
2014-04-08 17:52:00 +0200

    fio: fix s390 time accounting
    The current timer implementation could cause time warps on s390 which ends up
    as time bound jobs that would never end, because they always reset themself to
    the old time.
    
    When touching this code anyway, we also change it to use the faster stckf
    and avoid the calibration as we can control the result to be usecs.
    This also eliminates a few calculations cycle->usec in the hot path for the
    timer.
    
    In case other architectures have similar improved timers that might not be
    usec based, but nsec based or such a thing any architecture can set
    ARCH_CPU_CLOCK_CYCLES_PER_USEC to an appropriate per-arch value.
    This leaves the infrastructure open for others and the compiler will throw
    our division by 1 away anyway.
    
    Signed-off-by: Christian Ehrhardt <ehrhardt@linux.vnet.ibm.com>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- arch/arch-s390.h
+++ arch/arch-s390.h
@@ -22,13 +22,20 @@
 #define read_barrier()	asm volatile("bcr 15,0" : : : "memory")
 #define write_barrier()	asm volatile("bcr 15,0" : : : "memory")
 
+/*
+ * Fio needs monotonic (never lower), but not strict monotonic (never the same)
+ * so store clock fast is enough
+ */
 static inline unsigned long long get_cpu_clock(void)
 {
 	unsigned long long clk;
 
-	__asm__ __volatile__("stck %0" : "=Q" (clk) : : "cc");
-	return clk;
+	__asm__ __volatile__("stckf %0" : "=Q" (clk) : : "cc");
+	return clk>>12;
 }
+
+#define ARCH_CPU_CLOCK_CYCLES_PER_USEC 1
+#define ARCH_HAVE_CPU_CLOCK
 
 #define ARCH_HAVE_INIT
 extern int tsc_reliable;
@@ -37,7 +44,5 @@ static inline int arch_init(char *envp[])
 	tsc_reliable = 1;
 	return 0;
 }
-
-#define ARCH_HAVE_CPU_CLOCK
 
 #endif
