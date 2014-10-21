cd5902f976e1f86e360c0c46a0a5793560f9c9c8
Jens Axboe <axboe@fb.com>
2014-10-21 08:07:33 -0600

    compiler: improve gcc 4.x compile time check
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- compiler/compiler-gcc4.h
+++ compiler/compiler-gcc4.h
@@ -5,4 +5,13 @@
 #define __must_check		__attribute__((warn_unused_result))
 #endif
 
+#define GCC_VERSION (__GNUC__ * 10000		\
+			+ __GNUC_MINOR__ * 100	\
+			+ __GNUC_PATCHLEVEL__)
+
+#if GCC_VERSION >= 40300
+#define __compiletime_warning(message)	__attribute__((warning(message)))
+#define __compiletime_error(message)	__attribute__((error(message)))
+#endif
+
 #endif
