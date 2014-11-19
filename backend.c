796fb3ce83f0f890b8d5fb9c39b5524a6225e88c
Jens Axboe <axboe@fb.com>
2014-11-19 09:21:16 -0700

    trigger: always send trigger, not just when remote command is set
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- backend.c
+++ backend.c
@@ -1804,10 +1804,9 @@ void exec_trigger(const char *cmd)
 void check_trigger_file(void)
 {
 	if (__check_trigger_file() || trigger_timedout()) {
-		if (nr_clients) {
-			if (trigger_remote_cmd)
-				fio_clients_send_trigger(trigger_remote_cmd);
-		} else {
+		if (nr_clients)
+			fio_clients_send_trigger(trigger_remote_cmd);
+		else {
 			verify_save_state();
 			fio_terminate_threads(TERMINATE_ALL);
 			exec_trigger(trigger_cmd);
