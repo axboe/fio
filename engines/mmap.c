0162683724850b047a4f2687e3946443080e2079
Jens Axboe <axboe@fb.com>
2014-12-16 10:35:07 -0700

    engines/mmap: fix segfault on large devices and 32-bit archs
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- engines/mmap.c
+++ engines/mmap.c
@@ -103,6 +103,11 @@ static int fio_mmapio_prep_full(struct thread_data *td, struct io_u *io_u)
 
 	if (fio_file_partial_mmap(f))
 		return EINVAL;
+	if (io_u->offset != (size_t) io_u->offset ||
+	    f->io_size != (size_t) f->io_size) {
+		fio_file_set_partial_mmap(f);
+		return EINVAL;
+	}
 
 	fmd->mmap_sz = f->io_size;
 	fmd->mmap_off = 0;
