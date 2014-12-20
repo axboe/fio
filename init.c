axboe/fio

commit 1bf2498d311155bc4eb43edb4e6d3a04845d94c5

Always scramble buffers, if scramble_buffers is set
This makes scramble_buffers and zero_buffers cooperate nicely.

Signed-off-by: Jens Axboe <axboe@fb.com>

init.c
@@ -926,7 +926,13 @@ static void init_flags(struct thread_data *td)
 		td->flags |= TD_F_READ_IOLOG;
 	if (o->refill_buffers)
 		td->flags |= TD_F_REFILL_BUFFERS;
-	if (o->scramble_buffers)
+	/*
+	 * Scramble by default, but not if zero_buffer is true and has been
+	 * set. But if scramble_buffers has been set, always scramble.
+	 */
+	if (o->scramble_buffers && ((!o->zero_buffers &&
+	    fio_option_is_set(o, zero_buffers)) ||
+	    fio_option_is_set(o, scramble_buffers)))
 		td->flags |= TD_F_SCRAMBLE_BUFFERS;
 	if (o->verify != VERIFY_NONE)
 		td->flags |= TD_F_VER_NONE;

commit b04f590551050b23768a3969d47371d363aa8745

Fix default scrambling of buffers
Commit 1bf2498d3111 inadvertently made us not scramble by default.
The logic was a bit convoluted, so clear it up and scramble if:

- Asked so by adding scramble_buffers=1
- Or, if not asked to explicitly, but zero_buffers not set to 1.

Reported-by: Chris Wilson <Chris.Wilson@nutanix.com>
Signed-off-by: Jens Axboe <axboe@fb.com>

init.c
@@ -933,12 +933,16 @@ static void init_flags(struct thread_data *td)
 	if (o->refill_buffers)
 		td->flags |= TD_F_REFILL_BUFFERS;
 	/*
-	 * Scramble by default, but not if zero_buffer is true and has been
-	 * set. But if scramble_buffers has been set, always scramble.
+	 * Always scramble buffers if asked to
 	 */
-	if (o->scramble_buffers && ((!o->zero_buffers &&
-	    fio_option_is_set(o, zero_buffers)) ||
-	    fio_option_is_set(o, scramble_buffers)))
+	if (o->scramble_buffers && fio_option_is_set(o, scramble_buffers))
+		td->flags |= TD_F_SCRAMBLE_BUFFERS;
+	/*
+	 * But also scramble buffers, unless we were explicitly asked
+	 * to zero them.
+	 */
+	if (o->scramble_buffers && !(o->zero_buffers &&
+	    fio_option_is_set(o, zero_buffers)))
 		td->flags |= TD_F_SCRAMBLE_BUFFERS;
 	if (o->verify != VERIFY_NONE)
 		td->flags |= TD_F_VER_NONE;