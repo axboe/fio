axboe/fio

commit 12bde3697fc230d7af560cecc8ac469ab47643f8

axmap: ensure we lock down the maps for shared access
Signed-off-by: Jens Axboe <axboe@fb.com>

lib/axmap.c
@@ -23,6 +23,7 @@
 #include "../arch/arch.h"
 #include "axmap.h"
 #include "../smalloc.h"
+#include "../mutex.h"
 #include "../minmax.h"
 
 #if BITS_PER_LONG == 64
@@ -45,6 +46,7 @@ struct axmap_level {
 };
 
 struct axmap {
+	struct fio_mutex lock;
 	unsigned int nr_levels;
 	struct axmap_level *levels;
 	uint64_t first_free;
@@ -63,6 +65,8 @@ void axmap_reset(struct axmap *axmap)
 {
 	int i;
 
+	fio_mutex_down(&axmap->lock);
+
 	for (i = 0; i < axmap->nr_levels; i++) {
 		struct axmap_level *al = &axmap->levels[i];
 
@@ -70,6 +74,7 @@ void axmap_reset(struct axmap *axmap)
 	}
 
 	axmap->first_free = 0;
+	fio_mutex_up(&axmap->lock);
 }
 
 void axmap_free(struct axmap *axmap)
@@ -83,6 +88,7 @@ void axmap_free(struct axmap *axmap)
 		sfree(axmap->levels[i].map);
 
 	sfree(axmap->levels);
+	__fio_mutex_remove(&axmap->lock);
 	sfree(axmap);
 }
 
@@ -94,6 +100,8 @@ struct axmap *axmap_new(unsigned long nr_bits)
 	axmap = smalloc(sizeof(*axmap));
 	if (!axmap)
 		return NULL;
+
+	__fio_mutex_init(&axmap->lock, FIO_MUTEX_UNLOCKED);
 
 	levels = 1;
 	i = (nr_bits + BLOCKS_PER_UNIT - 1) >> UNIT_SHIFT;
@@ -126,6 +134,8 @@ err:
 			sfree(axmap->levels[i].map);
 
 	sfree(axmap->levels);
+	__fio_mutex_remove(&axmap->lock);
+	sfree(axmap);
 	return NULL;
 }
 
@@ -287,7 +297,9 @@ void axmap_set(struct axmap *axmap, uint64_t bit_nr)
 {
 	struct axmap_set_data data = { .nr_bits = 1, };
 
+	fio_mutex_down(&axmap->lock);
 	__axmap_set(axmap, bit_nr, &data);
+	fio_mutex_up(&axmap->lock);
 }
 
 unsigned int axmap_set_nr(struct axmap *axmap, uint64_t bit_nr, unsigned int nr_bits)
@@ -323,8 +335,14 @@ static int axmap_isset_fn(struct axmap_level *al, unsigned long offset,
 
 int axmap_isset(struct axmap *axmap, uint64_t bit_nr)
 {
-	if (bit_nr <= axmap->nr_bits)
-		return axmap_handler_topdown(axmap, bit_nr, axmap_isset_fn, NULL);
+	if (bit_nr <= axmap->nr_bits) {
+		int ret;
+
+		fio_mutex_down(&axmap->lock);
+		ret = axmap_handler_topdown(axmap, bit_nr, axmap_isset_fn, NULL);
+		fio_mutex_up(&axmap->lock);
+		return ret;
+	}
 
 	return 0;
 }
@@ -371,11 +389,17 @@ static uint64_t axmap_find_first_free(struct axmap *axmap, unsigned int level,
 
 uint64_t axmap_first_free(struct axmap *axmap)
 {
+	uint64_t ret;
+
 	if (firstfree_valid(axmap))
 		return axmap->first_free;
 
-	axmap->first_free = axmap_find_first_free(axmap, axmap->nr_levels - 1, 0);
-	return axmap->first_free;
+	fio_mutex_down(&axmap->lock);
+	ret = axmap_find_first_free(axmap, axmap->nr_levels - 1, 0);
+	axmap->first_free = ret;
+	fio_mutex_up(&axmap->lock);
+
+	return ret;
 }
 
 struct axmap_next_free_data {
@@ -411,11 +435,17 @@ uint64_t axmap_next_free(struct axmap *axmap, uint64_t bit_nr)
 	struct axmap_next_free_data data = { .level = -1U, .bit = bit_nr, };
 	uint64_t ret;
 
-	if (firstfree_valid(axmap) && bit_nr < axmap->first_free)
-		return axmap->first_free;
+	fio_mutex_down(&axmap->lock);
 
-	if (!axmap_handler(axmap, bit_nr, axmap_next_free_fn, &data))
-		return axmap_first_free(axmap);
+	if (firstfree_valid(axmap) && bit_nr < axmap->first_free) {
+		ret = axmap->first_free;
+		goto done;
+	}
+
+	if (!axmap_handler(axmap, bit_nr, axmap_next_free_fn, &data)) {
+		ret = axmap_first_free(axmap);
+		goto done;
+	}
 
 	assert(data.level != -1U);
 
@@ -425,8 +455,12 @@ uint64_t axmap_next_free(struct axmap *axmap, uint64_t bit_nr)
 	 * find the first free one, the map is practically full.
 	 */
 	ret = axmap_find_first_free(axmap, data.level, data.offset);
-	if (ret != -1ULL)
+	if (ret != -1ULL) {
+done:
+		fio_mutex_up(&axmap->lock);
 		return ret;
+	}
 
-	return axmap_first_free(axmap);
+	ret = axmap_first_free(axmap);
+	goto done;
 }

t/axmap.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <inttypes.h>
 
+#include "../mutex.h"
 #include "../lib/lfsr.h"
 #include "../lib/axmap.h"
 

Makefile
@@ -186,7 +186,7 @@ T_ZIPF_OBJS += t/log.o lib/ieee754.o lib/rand.o lib/zipf.o t/genzipf.o
 T_ZIPF_PROGS = t/fio-genzipf
 
 T_AXMAP_OBJS = t/axmap.o
-T_AXMAP_OBJS += lib/lfsr.o lib/axmap.o
+T_AXMAP_OBJS += lib/lfsr.o lib/axmap.o gettime.o mutex.o t/log.o t/debug.o
 T_AXMAP_PROGS = t/axmap
 
 T_LFSR_TEST_OBJS = t/lfsr-test.o

commit d555a23db84364ab942361a719a4379605bdbc41

axmap: fix deadlock
axmap_first_free() is always called with axmap->lock held,
so needn't to acquire the lock inside the function.

The deadlock is introduced in commit 12bde3697fc230d7a(axmap:
ensure we lock down the maps for shared access).

Given axmap_first_free() is only called inside lib/axmap.c,
this patch declares the function as static. In the future,
if external users need the function, axmap lock can be
considered at that time.

Reported-by: Sedat Dilek <sedat.dilek@gmail.com>
Signed-off-by: Ming Lei <ming.lei@canonical.com>
Tested-by: Sedat Dilek <sedat.dilek@gmail.com>
Signed-off-by: Jens Axboe <axboe@fb.com>

lib/axmap.c
@@ -387,17 +387,15 @@ static uint64_t axmap_find_first_free(struct axmap *axmap, unsigned int level,
 	return (uint64_t) -1ULL;
 }
 
-uint64_t axmap_first_free(struct axmap *axmap)
+static uint64_t axmap_first_free(struct axmap *axmap)
 {
 	uint64_t ret;
 
 	if (firstfree_valid(axmap))
 		return axmap->first_free;
 
-	fio_mutex_down(&axmap->lock);
 	ret = axmap_find_first_free(axmap, axmap->nr_levels - 1, 0);
 	axmap->first_free = ret;
-	fio_mutex_up(&axmap->lock);
 
 	return ret;
 }

lib/axmap.h
@@ -11,7 +11,6 @@ void axmap_clear(struct axmap *axmap, uint64_t bit_nr);
 void axmap_set(struct axmap *axmap, uint64_t bit_nr);
 unsigned int axmap_set_nr(struct axmap *axmap, uint64_t bit_nr, unsigned int nr_bits);
 int axmap_isset(struct axmap *axmap, uint64_t bit_nr);
-uint64_t axmap_first_free(struct axmap *axmap);
 uint64_t axmap_next_free(struct axmap *axmap, uint64_t bit_nr);
 void axmap_reset(struct axmap *axmap);
 