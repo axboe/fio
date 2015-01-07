axboe/fio

commit f893b76d5745811033f056ae4d4efe4f571452bd

backend: use monotonic clock for ETA, if we have it
Signed-off-by: Jens Axboe <axboe@fb.com>

backend.c
@@ -2104,11 +2104,19 @@ static void *helper_thread_main(void *data)
 		uint64_t sec = DISK_UTIL_MSEC / 1000;
 		uint64_t nsec = (DISK_UTIL_MSEC % 1000) * 1000000;
 		struct timespec ts;
+
+#if defined(CONFIG_CLOCK_MONOTONIC)
+		clock_gettime(CLOCK_MONOTONIC, &ts);
+		ts.tv_sec += sec;
+		ts.tv_nsec += nsec;
+#else
 		struct timeval tv;
 
 		gettimeofday(&tv, NULL);
 		ts.tv_sec = tv.tv_sec + sec;
 		ts.tv_nsec = (tv.tv_usec * 1000) + nsec;
+#endif
+
 		if (ts.tv_nsec >= 1000000000ULL) {
 			ts.tv_nsec -= 1000000000ULL;
 			ts.tv_sec++;

commit 36bb0880c7f7f48abb86e3a16d3168343dda9b75

Fix disk utils being updated too often
Fixes: f893b76d5745
Signed-off-by: Jens Axboe <axboe@fb.com>

backend.c
@@ -2104,18 +2104,11 @@ static void *helper_thread_main(void *data)
 		uint64_t sec = DISK_UTIL_MSEC / 1000;
 		uint64_t nsec = (DISK_UTIL_MSEC % 1000) * 1000000;
 		struct timespec ts;
-
-#if defined(CONFIG_CLOCK_MONOTONIC)
-		clock_gettime(CLOCK_MONOTONIC, &ts);
-		ts.tv_sec += sec;
-		ts.tv_nsec += nsec;
-#else
 		struct timeval tv;
 
 		gettimeofday(&tv, NULL);
 		ts.tv_sec = tv.tv_sec + sec;
 		ts.tv_nsec = (tv.tv_usec * 1000) + nsec;
-#endif
 
 		if (ts.tv_nsec >= 1000000000ULL) {
 			ts.tv_nsec -= 1000000000ULL;