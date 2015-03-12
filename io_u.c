95e564523d502e5a682ae7f6ee793ca4c9ca76ca
Jens Axboe <axboe@fb.com>
2015-03-12 10:05:25 -0600

    rate: remove redundant code in rate_ddir()
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- io_u.c
+++ io_u.c
@@ -590,9 +590,6 @@ static enum fio_ddir rate_ddir(struct thread_data *td, enum fio_ddir ddir)
 	if (td_rw(td) && __should_check_rate(td, odir))
 		td->rate_pending_usleep[odir] -= usec;
 
-	if (ddir == DDIR_TRIM)
-		return DDIR_TRIM;
-
 	return ddir;
 }
 
