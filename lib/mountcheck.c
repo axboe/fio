aae599ba27e581c6492f9bcbd7119a4a25ee1a03
Jens Axboe <axboe@fb.com>
2015-05-21 21:19:14 -0600

    Add start of mount check
    For raw block devices, lets add some safety check that will check for
    destructive tests whether a device is mounted or not. Will plumb this
    in soon, and add an override option to continue regardless.
    
    Signed-off-by: Jens Axboe <axboe@fb.com>

---
+++ lib/mountcheck.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <mntent.h>
+
+#ifdef CONFIG_GETMNTENT
+
+#define MTAB	"/etc/mtab"
+
+int device_is_mounted(const char *dev)
+{
+	FILE *mtab;
+	struct mntent *mnt;
+	int ret = 0;
+
+	mtab = setmntent(MTAB, "r");
+	if (!mtab)
+		return 0;
+
+	while ((mnt = getmntent(mtab)) != NULL) {
+		if (!mnt->mnt_fsname)
+			continue;
+		if (!strcmp(mnt->mnt_fsname, dev)) {
+			ret = 1;
+			break;
+		}
+	}
+
+	endmntent(mtab);
+	return ret;
+}
+
+#else
+
+int device_is_mounted(const char *dev)
+{
+	return 0;
+}
+
+#endif
