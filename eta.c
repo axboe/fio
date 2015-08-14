cce8514585480bfff0774f285efe633ed1bb1d84
Jens Axboe <axboe@fb.com>
2015-08-14 09:42:55 -0600

    eta: ensure we include terminating 0 space in je->run_str[]
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- eta.c
+++ eta.c
@@ -590,7 +590,7 @@ struct jobs_eta *get_jobs_eta(int force, size_t *size)
 	if (!thread_number)
 		return NULL;
 
-	*size = sizeof(*je) + THREAD_RUNSTR_SZ;
+	*size = sizeof(*je) + THREAD_RUNSTR_SZ + 1;
 	je = malloc(*size);
 	if (!je)
 		return NULL;
