axboe/fio

commit b01af66b13f0594617602b61d31b1495292e5fd2

add eta and elapsed to root of json output

stat.c
@@ -1062,16 +1062,25 @@ static struct json_object *show_thread_status_json(struct thread_stat *ts,
 				    struct group_run_stats *rs)
 {
 	struct json_object *root, *tmp;
+	struct jobs_eta *je;
 	double io_u_dist[FIO_IO_U_MAP_NR];
 	double io_u_lat_u[FIO_IO_U_LAT_U_NR];
 	double io_u_lat_m[FIO_IO_U_LAT_M_NR];
 	double usr_cpu, sys_cpu;
 	int i;
+	size_t size;
+
 
 	root = json_create_object();
 	json_object_add_value_string(root, "jobname", ts->name);
 	json_object_add_value_int(root, "groupid", ts->groupid);
 	json_object_add_value_int(root, "error", ts->error);
+
+	/* ETA Info */
+	je = get_jobs_eta(1, &size);
+	json_object_add_value_int(root, "eta", je->eta_sec);
+	json_object_add_value_int(root, "elapsed", je->elapsed_sec);
+
 
 	add_ddir_status_json(ts, rs, DDIR_READ, root);
 	add_ddir_status_json(ts, rs, DDIR_WRITE, root);

commit 2de615ad34e323ec7c8f0f595ed4dc5124f99a42

stat: fix potential segfault for json output on network client
Fixes: b01af66b13f0 ("add eta and elapsed to root of json output")
Signed-off-by: Jens Axboe <axboe@fb.com>

stat.c
@@ -1083,9 +1083,10 @@ static struct json_object *show_thread_status_json(struct thread_stat *ts,
 
 	/* ETA Info */
 	je = get_jobs_eta(1, &size);
-	json_object_add_value_int(root, "eta", je->eta_sec);
-	json_object_add_value_int(root, "elapsed", je->elapsed_sec);
-
+	if (je) {
+		json_object_add_value_int(root, "eta", je->eta_sec);
+		json_object_add_value_int(root, "elapsed", je->elapsed_sec);
+	}
 
 	add_ddir_status_json(ts, rs, DDIR_READ, root);
 	add_ddir_status_json(ts, rs, DDIR_WRITE, root);