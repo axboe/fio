axboe/fio

commit a2e6f8ac56a9b0f519ccc6cd13e76a9b239ba37d

Make iodepth_batch=1 by default
Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

options.c
@@ -504,6 +504,8 @@ static struct fio_option options[] = {
 		.off1	= td_var_offset(iodepth_batch),
 		.help	= "Number of IO to submit in one go",
 		.parent	= "iodepth",
+		.minval	= 1,
+		.def	= "1",
 	},
 	{
 		.name	= "iodepth_low",

commit 3482263a88c18df1f2d475f426d947e266d22e10

Fix iodepth_batch=0
The man page claims that iodepth_batch=0 falls back to whatever was
specified for iodepth, but the enforced minimum of 1 means that 0 is not
actually valid.

Fixes: a2e6f8ac56a9 ("Make iodepth_batch=1 by default")
Signed-off-by: Omar Sandoval <osandov@fb.com>
Signed-off-by: Jens Axboe <axboe@fb.com>

options.c
@@ -1678,7 +1678,6 @@ struct fio_option fio_options[FIO_MAX_OPTS] = {
 		.help	= "Number of IO buffers to submit in one go",
 		.parent	= "iodepth",
 		.hide	= 1,
-		.minval	= 1,
 		.interval = 1,
 		.def	= "1",
 		.category = FIO_OPT_C_IO,