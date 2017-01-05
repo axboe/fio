d0c8ccb05060d76e74a6320449ef2e4f26a7797b
Jens Axboe <axboe@fb.com>
2017-01-05 09:38:50 -0700

    verify: use log_verify_failure() for pattern verifies
    Similar to the other verify types, otherwise we don't get
    the absolute offset logged.
    
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- verify.c
+++ verify.c
@@ -393,7 +393,7 @@ static int verify_io_u_pattern(struct verify_header *hdr, struct vcont *vc)
 				(unsigned char)pattern[mod],
 				bits);
 			log_err("fio: bad pattern block offset %u\n", i);
-			dump_verify_buffers(hdr, vc);
+			log_verify_failure(hdr, vc);
 			return EILSEQ;
 		}
 		mod++;
