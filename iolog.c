7abe6a27549dd484c97daaa613bcb8e37e6c98f0
Tomohiro Kusumi <tkusumi@tuxera.com>
2017-01-11 02:39:45 +0900

    Add missing trailing \n in dprint()
    Signed-off-by: Tomohiro Kusumi <tkusumi@tuxera.com>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- iolog.c
+++ iolog.c
@@ -277,7 +277,7 @@ restart:
 			overlap = 1;
 
 		if (overlap) {
-			dprint(FD_IO, "iolog: overlap %llu/%lu, %llu/%lu",
+			dprint(FD_IO, "iolog: overlap %llu/%lu, %llu/%lu\n",
 				__ipo->offset, __ipo->len,
 				ipo->offset, ipo->len);
 			td->io_hist_len--;
