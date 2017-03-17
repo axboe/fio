d9bb03d475e918e08c38bd882032ff788daa297f
Jens Axboe <axboe@fb.com>
2017-03-17 10:39:42 -0600

    is_power_of_2() should return bool
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- lib/pow2.h
+++ lib/pow2.h
@@ -2,8 +2,9 @@
 #define FIO_POW2_H
 
 #include <inttypes.h>
+#include "types.h"
 
-static inline int is_power_of_2(uint64_t val)
+static inline bool is_power_of_2(uint64_t val)
 {
 	return (val != 0 && ((val & (val - 1)) == 0));
 }
