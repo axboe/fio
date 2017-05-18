axboe/fio

commit 7b5c648f6a5c05716e2f345c9d640965abda8761

Fix wrap issue with 64-bit pwritev2/preadv2
Apparently I missed that Linux does a clever trick to optimize
how we pass in lo/hi offsets for 64-bit, and it encodes the
full offset in just the low part. This caused corruption when
writing with pwritev2 for laver sizes, and bad data for preadv2
for larger sizes.

Signed-off-by: Jens Axboe <axboe@fb.com>

os/os-linux.h
@@ -314,9 +314,15 @@ static inline int fio_set_sched_idle(void)
 static inline void make_pos_h_l(unsigned long *pos_h, unsigned long *pos_l,
 				off_t offset)
 {
+#if BITS_PER_LONG == 64
+#warning 64
+	*pos_l = offset;
+	*pos_h = 0;
+#else
+#warning 32
 	*pos_l = offset & 0xffffffff;
 	*pos_h = ((uint64_t) offset) >> 32;
-
+#endif
 }
 static inline ssize_t preadv2(int fd, const struct iovec *iov, int iovcnt,
 			      off_t offset, unsigned int flags)

commit dd3805d49995e59fdf61e2560c3fec5b7f5c71b6

Remove leftover warnings
Previous commit that fixed up the preadv2/pwritev2 wrappers
left some debug #warning in there by accident. Kill them.

Fixes: 7b5c648f6a5c ("Fix wrap issue with 64-bit pwritev2/preadv2")
Signed-off-by: Jens Axboe <axboe@fb.com>

os/os-linux.h
@@ -315,11 +315,9 @@ static inline void make_pos_h_l(unsigned long *pos_h, unsigned long *pos_l,
 				off_t offset)
 {
 #if BITS_PER_LONG == 64
-#warning 64
 	*pos_l = offset;
 	*pos_h = 0;
 #else
-#warning 32
 	*pos_l = offset & 0xffffffff;
 	*pos_h = ((uint64_t) offset) >> 32;
 #endif