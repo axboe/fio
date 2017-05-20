15e0d945776175b179f0bf4373dff784722251d3
Omri Mor <omor1@asu.edu>
2017-05-20 01:55:34 -0700

    flist.h: replace offsetof macros by stddef.h include
    Signed-off-by: Omri Mor <omor1@asu.edu>

--- flist.h
+++ flist.h
@@ -2,13 +2,7 @@
 #define _LINUX_FLIST_H
 
 #include <stdlib.h>
-
-#undef offsetof
-#ifdef __compiler_offsetof
-#define offsetof(TYPE,MEMBER) __compiler_offsetof(TYPE,MEMBER)
-#else
-#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
-#endif
+#include <stddef.h>
 
 #define container_of(ptr, type, member) ({			\
 	const typeof( ((type *)0)->member ) *__mptr = (ptr);	\
