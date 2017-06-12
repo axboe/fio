052046a1e92bff19ecae277670a59fb726a66b86
Tomohiro Kusumi <tkusumi@tuxera.com>
2017-06-12 23:02:11 +0300

    binject: don't use void* for pointer arithmetic (gcc)
    I think gcc extension should be avoided when it can be done by
    just changing a pointer type.
    
    Signed-off-by: Tomohiro Kusumi <tkusumi@tuxera.com>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- engines/binject.c
+++ engines/binject.c
@@ -59,11 +59,12 @@ static int pollin_events(struct pollfd *pfds, int fds)
 	return 0;
 }
 
-static unsigned int binject_read_commands(struct thread_data *td, void *p,
+static unsigned int binject_read_commands(struct thread_data *td, void *buf,
 					  int left, int *err)
 {
 	struct fio_file *f;
 	int i, ret, events;
+	char *p = buf;
 
 one_more:
 	events = 0;
