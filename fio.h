b2fcbe01bdac01bc5d7f8ddea94f264b9f8c2003
Omri Mor <omor1@asu.edu>
2017-06-19 16:41:51 -0600

    Ensure that thread_stat alignment is correct
    clang seems to align this weirdly, and then fault when it
    isn't 128-bit aligned. Align it explicitly, so it does the
    right thing.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fio.h
+++ fio.h
@@ -149,7 +149,7 @@ struct thread_data {
 	unsigned int thread_number;
 	unsigned int subjob_number;
 	unsigned int groupid;
-	struct thread_stat ts __attribute__ ((aligned));
+	struct thread_stat ts __attribute__ ((aligned(8)));
 
 	int client_type;
 
