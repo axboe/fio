axboe/fio

commit 17373ce2f38aa0d788d5c604cefd36a367bbbc27

io_u: wrap to beginning when end-of-file is reached for time_based
The logic around using io_size isn't correct.

Signed-off-by: Jens Axboe <axboe@kernel.dk>

io_u.c
@@ -361,16 +361,13 @@ static int get_next_seq_offset(struct thread_data *td, struct fio_file *f,
 
 	assert(ddir_rw(ddir));
 
+	/*
+	 * If we reach the end for a time based run, reset us back to 0
+	 * and invalidate the cache, if we need to.
+	 */
 	if (f->last_pos[ddir] >= f->io_size + get_start_offset(td, f) &&
 	    o->time_based) {
-		struct thread_options *o = &td->o;
-		uint64_t io_size = f->io_size + (f->io_size % o->min_bs[ddir]);
-
-		if (io_size > f->last_pos[ddir])
-			f->last_pos[ddir] = 0;
-		else
-			f->last_pos[ddir] = f->last_pos[ddir] - io_size;
-
+		f->last_pos[ddir] = 0;
 		loop_cache_invalidate(td, f);
 	}
 

commit c89daa4a98e6f3749ffc75b727a77cc061a0a454

io_u: reset file to initial offset
Don't assume that initial offset is 0, we should use the set
f->file_offset when resetting.

Fixes: 17373ce2f38a ("io_u: wrap to beginning when end-of-file is reached for time_based")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

io_u.c
@@ -367,7 +367,7 @@ static int get_next_seq_offset(struct thread_data *td, struct fio_file *f,
 	 */
 	if (f->last_pos[ddir] >= f->io_size + get_start_offset(td, f) &&
 	    o->time_based) {
-		f->last_pos[ddir] = 0;
+		f->last_pos[ddir] = f->file_offset;
 		loop_cache_invalidate(td, f);
 	}
 