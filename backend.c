axboe/fio

commit 1aa39b0ce447f228460e6d0af601fee88fd5f4b4

rate: ensure IO issue restarts right after sleep
If we have rate_ignore_thinktime set, we account the number of bytes
we would have done in the period of the sleep. However, we need
to subtract one block, otherwise we end up doing a rated sleep
right after the thinktime sleep.

Fixes: 1a9bf814 ("Add option to ignore thinktime for rated IO")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

backend.c
@@ -894,10 +894,13 @@ static void handle_thinktime(struct thread_data *td, enum fio_ddir ddir)
 
 	/*
 	 * If we're ignoring thinktime for the rate, add the number of bytes
-	 * we would have done while sleeping.
+	 * we would have done while sleeping, minus one block to ensure we
+	 * start issuing immediately after the sleep.
 	 */
-	if (total && td->rate_bps[ddir] && td->o.rate_ign_think)
+	if (total && td->rate_bps[ddir] && td->o.rate_ign_think) {
 		td->rate_io_issue_bytes[ddir] += (td->rate_bps[ddir] * 1000000) / total;
+		td->rate_io_issue_bytes[ddir] -= td->o.min_bs[ddir];
+	}
 }
 
 /*

commit f2aedb171c4feaae028645bd0ddb2d8cd25bfaa5

rate: fix bad math
To figure out how much data we missed when doing a thinktime sleep,
we're currently dividing by the time slept. This is wrong, it should
be multiplied by the time slept and divided by 1000000 to go from
usec to a second base.

Additionally, don't ever subtract more than a block of data, and
adjust down depending on sleep.

Fixes: 1aa39b0c ("rate: ensure IO issue restarts right after sleep")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

backend.c
@@ -898,8 +898,15 @@ static void handle_thinktime(struct thread_data *td, enum fio_ddir ddir)
 	 * start issuing immediately after the sleep.
 	 */
 	if (total && td->rate_bps[ddir] && td->o.rate_ign_think) {
-		td->rate_io_issue_bytes[ddir] += (td->rate_bps[ddir] * 1000000) / total;
-		td->rate_io_issue_bytes[ddir] -= td->o.min_bs[ddir];
+		uint64_t missed = (td->rate_bps[ddir] * total) / 1000000ULL;
+		uint64_t over;
+
+		if (total >= 1000000)
+			over = td->o.min_bs[ddir];
+		else
+			over = (td->o.min_bs[ddir] * total) / 1000000ULL;
+
+		td->rate_io_issue_bytes[ddir] += (missed - over);
 	}
 }
 