axboe/fio

commit 5ec3bc5401f354444bdc0ce3c240df664f50d520

init: add global 'warned' state
In various places we spew a warning if some static variable
isn't set, but this still means it can happen once per job.
Add some global state to allow to track this globally instead.

Signed-off-by: Jens Axboe <axboe@kernel.dk>

debug.h
@@ -2,6 +2,7 @@
 #define FIO_DEBUG_H
 
 #include <assert.h>
+#include "lib/types.h"
 #include "log.h"
 
 enum {
@@ -26,7 +27,17 @@ enum {
 	FD_DEBUG_MAX,
 };
 
-extern unsigned int fio_debug_jobno, *fio_debug_jobp;
+extern unsigned int fio_debug_jobno, *fio_debug_jobp, *fio_warned;
+
+static inline bool fio_did_warn(unsigned int mask)
+{
+	if (!(*fio_warned & mask)) {
+		*fio_warned |= mask;
+		return true;
+	}
+
+	return false;
+}
 
 #ifdef FIO_INC_DEBUG
 struct debug_level {

init.c
@@ -79,6 +79,7 @@ static int prev_group_jobs;
 unsigned long fio_debug = 0;
 unsigned int fio_debug_jobno = -1;
 unsigned int *fio_debug_jobp = NULL;
+unsigned int *fio_warned = NULL;
 
 static char cmd_optstr[256];
 static bool did_arg;
@@ -309,6 +310,7 @@ static void free_shm(void)
 	if (threads) {
 		flow_exit();
 		fio_debug_jobp = NULL;
+		fio_warned = NULL;
 		free_threads_shm();
 	}
 
@@ -341,7 +343,7 @@ static int setup_thread_area(void)
 	do {
 		size_t size = max_jobs * sizeof(struct thread_data);
 
-		size += sizeof(unsigned int);
+		size += 2 * sizeof(unsigned int);
 
 #ifndef CONFIG_NO_SHM
 		shm_id = shmget(0, size, IPC_CREAT | 0600);
@@ -376,6 +378,8 @@ static int setup_thread_area(void)
 	memset(threads, 0, max_jobs * sizeof(struct thread_data));
 	fio_debug_jobp = (unsigned int *)(threads + max_jobs);
 	*fio_debug_jobp = -1;
+	fio_warned = fio_debug_jobp + 1;
+	*fio_warned = 0;
 
 	flow_init();
 

commit f2cd91604af170e972438c461a40230e266a57d9

debug: fix inverted logic in fio_did_warn()
Fixes: 5ec3bc5401f3 ("init: add global 'warned' state")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

debug.h
@@ -31,11 +31,10 @@ extern unsigned int fio_debug_jobno, *fio_debug_jobp, *fio_warned;
 
 static inline bool fio_did_warn(unsigned int mask)
 {
-	if (!(*fio_warned & mask)) {
-		*fio_warned |= mask;
+	if (*fio_warned & mask)
 		return true;
-	}
 
+	*fio_warned |= mask;
 	return false;
 }
 