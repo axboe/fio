3a62cbace68d4c55a43d47dc9f4ef052a63bc0af
Sitsofe Wheeler <sitsofe@yahoo.com>
2018-02-21 14:41:14 +0000

    windows: minor windows installer improvements
    - Stop hard coding fio version in the Windows installer build file and try and
      use the generated version instead
    - Make installer build use sys.BUILDARCH rather than env.FIO_ARCH
    - Change IO to I/O in descriptions to match other packages
    
    Signed-off-by: Sitsofe Wheeler <sitsofe@yahoo.com>

--- os/windows/dobuild.cmd
+++ os/windows/dobuild.cmd
@@ -6,6 +6,16 @@ for /f "tokens=3" %%i in (..\..\FIO-VERSION-FILE) do (
  set /a counter+=1
 )
 
+for /f "tokens=2 delims=-" %%i in ("%FIO_VERSION%") do (
+ set FIO_VERSION_NUMBERS=%%i
+)
+
+if not defined FIO_VERSION_NUMBERS (
+  echo Could not find version numbers in the string '%FIO_VERSION%'
+  echo Expected version to follow format 'fio-^([0-9]+.[0-9.]+^)'
+  goto end
+)
+
 if "%1"=="x86" set FIO_ARCH=x86
 if "%1"=="x64" set FIO_ARCH=x64
 
@@ -16,7 +26,7 @@ if not defined FIO_ARCH (
   goto end
 )
 
-"%WIX%bin\candle" -nologo -arch %FIO_ARCH% install.wxs
+"%WIX%bin\candle" -nologo -arch %FIO_ARCH% -dFioVersionNumbers="%FIO_VERSION_NUMBERS%" install.wxs
 @if ERRORLEVEL 1 goto end
 "%WIX%bin\candle" -nologo -arch %FIO_ARCH% examples.wxs
 @if ERRORLEVEL 1 goto end
