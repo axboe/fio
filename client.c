axboe/fio

commit ec9e13345f3f433f3693891864cf794206cdd057

client: check return of dup(2)
If dup(2) fails, we can't pass it to fdopen.

Fixes: b4f5e72f1383 ("client: parse env variables before sending job-file contents to server")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

client.c
@@ -123,8 +123,13 @@ static int read_ini_data(int fd, void *data, size_t size)
 	char *p = data;
 	int ret = 0;
 	FILE *fp;
+	int dupfd;
 
-	fp = fdopen(dup(fd), "r");
+	dupfd = dup(fd);
+	if (dupfd < 0)
+		return errno;
+
+	fp = fdopen(dupfd, "r");
 	if (!fp)
 		return errno;
 

commit af84cd66149507424814cf9c0b4950f4cf66e3b7

client: close dup'ed descriptor if fdopen() fails
The saga continues... Ensure that we close the dup'ed descriptor,
if we give up due to fdopen() failure.

Reported-by: Bart Van Assche <bart.vanassche@wdc.com>
Fixes: ec9e13345f3f ("client: check return of dup(2)")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

client.c
@@ -130,8 +130,11 @@ static int read_ini_data(int fd, void *data, size_t size)
 		return errno;
 
 	fp = fdopen(dupfd, "r");
-	if (!fp)
-		return errno;
+	if (!fp) {
+		ret = errno;
+		close(dupfd);
+		goto out;
+	}
 
 	while (1) {
 		ssize_t len;
@@ -163,6 +166,7 @@ static int read_ini_data(int fd, void *data, size_t size)
 	}
 
 	fclose(fp);
+out:
 	return ret;
 }
 