196ccc44e70333cc87f9d417967bd06288602dac
Vincent Fu <vincent.fu@wdc.com>
2018-06-15 15:00:25 -0600

    fio.h: also check trim operations in fio_ro_check
    IO engines call fio_ro_check to ensure that the data direction
    set in the thread data structure permits writes before proceeding
    with a write io_u. This patch makes fio_ro_check ensure that the
    data direction also permits trims before proceeding with a trim
    io_u.

--- fio.h
+++ fio.h
@@ -533,7 +533,8 @@ extern bool eta_time_within_slack(unsigned int time);
 
 static inline void fio_ro_check(const struct thread_data *td, struct io_u *io_u)
 {
-	assert(!(io_u->ddir == DDIR_WRITE && !td_write(td)));
+	assert(!(io_u->ddir == DDIR_WRITE && !td_write(td)) &&
+	       !(io_u->ddir == DDIR_TRIM && !td_trim(td)));
 }
 
 #define REAL_MAX_JOBS		4096
