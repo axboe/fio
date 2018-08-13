922e390f69c8169a215187ea8ef5aca0deac11b8
Jens Axboe <axboe@kernel.dk>
2018-08-13 11:18:09 -0600

    asprintf: fix indentation
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- oslib/asprintf.c
+++ oslib/asprintf.c
@@ -6,38 +6,38 @@
 #ifndef CONFIG_HAVE_VASPRINTF
 int vasprintf(char **strp, const char *fmt, va_list ap)
 {
-    va_list ap_copy;
-    char *str;
-    int len;
+	va_list ap_copy;
+	char *str;
+	int len;
 
 #ifdef va_copy
-    va_copy(ap_copy, ap);
+	va_copy(ap_copy, ap);
 #else
-    __va_copy(ap_copy, ap);
+	__va_copy(ap_copy, ap);
 #endif
-    len = vsnprintf(NULL, 0, fmt, ap_copy);
-    va_end(ap_copy);
+	len = vsnprintf(NULL, 0, fmt, ap_copy);
+	va_end(ap_copy);
 
-    if (len < 0)
-        return len;
+	if (len < 0)
+		return len;
 
-    len++;
-    str = malloc(len);
-    *strp = str;
-    return str ? vsnprintf(str, len, fmt, ap) : -1;
+	len++;
+	str = malloc(len);
+	*strp = str;
+	return str ? vsnprintf(str, len, fmt, ap) : -1;
 }
 #endif
 
 #ifndef CONFIG_HAVE_ASPRINTF
 int asprintf(char **strp, const char *fmt, ...)
 {
-    va_list arg;
-    int done;
+	va_list arg;
+	int done;
 
-    va_start(arg, fmt);
-    done = vasprintf(strp, fmt, arg);
-    va_end(arg);
+	va_start(arg, fmt);
+	done = vasprintf(strp, fmt, arg);
+	va_end(arg);
 
-    return done;
+	return done;
 }
 #endif
