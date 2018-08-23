axboe/fio

commit 15a4f4962fad9e1777bcc662b3391f521ace8c5b

lib/axmap: Simplify axmap_set_fn()
Instead of handling ffz(~overlap) == bit as a special case, handle
this case in the same way as ffz(~overlap) != bit. This patch does
not change any functionality but removes one if-instruction from the
hot path.

Signed-off-by: Bart Van Assche <bart.vanassche@wdc.com>

lib/axmap.c
@@ -235,18 +235,12 @@ static bool axmap_set_fn(struct axmap_level *al, unsigned long offset,
 	 */
 	overlap = al->map[offset] & mask;
 	if (overlap == mask) {
-done:
 		data->set_bits = 0;
 		return true;
 	}
 
 	if (overlap) {
-		const int __bit = ffz(~overlap);
-
-		nr_bits = __bit - bit;
-		if (!nr_bits)
-			goto done;
-
+		nr_bits = ffz(~overlap) - bit;
 		mask = bit_masks[nr_bits] << bit;
 	}
 

commit 9ae329316cf24037894ffa79487ecb63d54a8613

lib/axmap: Remove an assert() statement that is no longer correct
Due to patch "lib/axmap: Simplify axmap_set_fn()" it is now possible
that mask is zero. Avoid that this causes fio to abort.

Fixes: 15a4f4962fad ("lib/axmap: Simplify axmap_set_fn()")
Signed-off-by: Bart Van Assche <bart.vanassche@wdc.com>

lib/axmap.c
@@ -244,7 +244,6 @@ static bool axmap_set_fn(struct axmap_level *al, unsigned long offset,
 		mask = bit_masks[nr_bits] << bit;
 	}
 
-	assert(mask);
 	assert(!(al->map[offset] & mask));
 	al->map[offset] |= mask;
 