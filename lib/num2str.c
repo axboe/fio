axboe/fio

commit 1ec3d69b0ed8cc7a3eba0192685034d5442989e4

Implement a better num2str()
Signed-off-by: Jens Axboe <jaxboe@fusionio.com>

lib/num2str.c
@@ -1,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/*
+ * Cheesy number->string conversion, complete with carry rounding error.
+ */
+char *num2str(unsigned long num, int maxlen, int base, int pow2)
+{
+	char postfix[] = { ' ', 'K', 'M', 'G', 'P', 'E' };
+	unsigned int thousand[] = { 1000, 1024 };
+	unsigned int modulo, decimals;
+	int post_index;
+	char tmp[32], fmt[8];
+	char *buf;
+
+	buf = malloc(128);
+
+	for (post_index = 0; base > 1; post_index++)
+		base /= thousand[!!pow2];
+
+	modulo = -1U;
+	while (post_index < sizeof(postfix)) {
+		sprintf(tmp, "%lu", num);
+		if (strlen(tmp) <= maxlen)
+			break;
+
+		modulo = num % thousand[!!pow2];
+		num /= thousand[!!pow2];
+		post_index++;
+	}
+
+	if (modulo == -1U) {
+done:
+		sprintf(buf, "%lu%c", num, postfix[post_index]);
+		return buf;
+	}
+
+	sprintf(tmp, "%lu", num);
+	decimals = maxlen - strlen(tmp);
+	if (decimals <= 1)
+		goto done;
+
+	sprintf(fmt, "%%.%uu", decimals - 1);
+	sprintf(tmp, fmt, modulo);
+	sprintf(buf, "%lu.%s%c", num, tmp, postfix[post_index]);
+	return buf;
+}

Makefile
@@ -11,6 +11,7 @@ OBJS = gettime.o fio.o ioengines.o init.o stat.o log.o time.o filesetup.o \
 
 OBJS += lib/rand.o
 OBJS += lib/flist_sort.o
+OBJS += lib/num2str.o
 
 OBJS += crc/crc7.o
 OBJS += crc/crc16.o

Makefile.FreeBSD
@@ -10,6 +10,7 @@ OBJS = gettime.o fio.o ioengines.o init.o stat.o log.o time.o filesetup.o \
 
 OBJS += lib/rand.o
 OBJS += lib/flist_sort.o
+OBJS += lib/num2str.o
 
 OBJS += crc/crc7.o
 OBJS += crc/crc16.o

Makefile.NetBSD
@@ -10,6 +10,7 @@ OBJS = gettime.o fio.o ioengines.o init.o stat.o log.o time.o filesetup.o \
 
 OBJS += lib/rand.o
 OBJS += lib/flist_sort.o
+OBJS += lib/num2str.o
 
 OBJS += crc/crc7.o
 OBJS += crc/crc16.o

Makefile.aix
@@ -11,6 +11,7 @@ OBJS = gettime.o fio.o ioengines.o init.o stat.o log.o time.o filesetup.o \
 
 OBJS += lib/rand.o
 OBJS += lib/getopt_long.o
+OBJS += lib/num2str.o
 
 OBJS += crc/crc7.o
 OBJS += crc/crc16.o

Makefile.mac
@@ -10,6 +10,7 @@ OBJS = gettime.o fio.o ioengines.o init.o stat.o log.o time.o filesetup.o \
 
 OBJS += lib/rand.o
 OBJS += lib/flist_sort.o
+OBJS += lib/num2str.o
 
 OBJS += crc/crc7.o
 OBJS += crc/crc16.o

Makefile.solaris
@@ -9,6 +9,7 @@ OBJS = gettime.o fio.o ioengines.o init.o stat.o log.o time.o filesetup.o \
 
 OBJS += lib/rand.o
 OBJS += lib/flist_sort.o
+OBJS += lib/num2str.o
 
 OBJS += crc/crc7.o
 OBJS += crc/crc16.o

fio.h
@@ -568,6 +568,8 @@ extern void options_mem_dupe(struct thread_data *);
 extern void options_mem_free(struct thread_data *);
 extern void td_fill_rand_seeds(struct thread_data *);
 extern void add_job_opts(const char **);
+extern char *num2str(unsigned long, int, int, int);
+
 #define FIO_GETOPT_JOB		0x89988998
 #define FIO_NR_OPTIONS		(FIO_MAX_OPTS + 128)
 
@@ -651,49 +653,6 @@ static inline int fio_fill_issue_time(struct thread_data *td)
 		return 1;
 
 	return 0;
-}
-
-/*
- * Cheesy number->string conversion, complete with carry rounding error.
- */
-static inline char *num2str(unsigned long num, int maxlen, int base, int pow2)
-{
-	char postfix[] = { ' ', 'K', 'M', 'G', 'P', 'E' };
-	unsigned int thousand;
-	char *buf;
-	int i;
-
-	if (pow2)
-		thousand = 1024;
-	else
-		thousand = 1000;
-
-	buf = malloc(128);
-
-	for (i = 0; base > 1; i++)
-		base /= thousand;
-
-	do {
-		int len, carry = 0;
-
-		len = sprintf(buf, "%'lu", num);
-		if (len <= maxlen) {
-			if (i >= 1) {
-				buf[len] = postfix[i];
-				buf[len + 1] = '\0';
-			}
-			return buf;
-		}
-
-		if ((num % thousand) >= (thousand / 2))
-			carry = 1;
-
-		num /= thousand;
-		num += carry;
-		i++;
-	} while (i <= 5);
-
-	return buf;
 }
 
 static inline int __should_check_rate(struct thread_data *td,

commit d0b2501a379dcf25cfcfe7059e9dc54b9c5ada4f

num2str(): Avoid an out-of-bounds array access
This patch fixes Coverity ID 169307.

Fixes: 1ec3d69b0ed8 ("Implement a better num2str()")
Signed-off-by: Bart Van Assche <bvanassche@acm.org>

lib/num2str.c
@@ -72,7 +72,7 @@ char *num2str(uint64_t num, int maxlen, int base, int pow2, enum n2s_unit units)
 	 * Divide by K/Ki until string length of num <= maxlen.
 	 */
 	modulo = -1U;
-	while (post_index < sizeof(sistr)) {
+	while (post_index < ARRAY_SIZE(sistr)) {
 		sprintf(tmp, "%llu", (unsigned long long) num);
 		if (strlen(tmp) <= maxlen)
 			break;