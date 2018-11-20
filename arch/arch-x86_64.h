axboe/fio

commit ebec344dd336784fe8c15fa042649a5e768af63d

engines/libaio: update to new io_setup2() system call
We need that to enable polling.

Signed-off-by: Jens Axboe <axboe@kernel.dk>

arch/arch-x86_64.h
@@ -1,6 +1,10 @@
 #ifndef ARCH_X86_64_H
 #define ARCH_X86_64_H
 
+#ifndef __NR_sys_iosetup2
+#define __NR_sys_iosetup2	335
+#endif
+
 static inline void do_cpuid(unsigned int *eax, unsigned int *ebx,
 			    unsigned int *ecx, unsigned int *edx)
 {

engines/libaio.c
@@ -16,6 +16,9 @@
 #ifndef IOCB_FLAG_HIPRI
 #define IOCB_FLAG_HIPRI	(1 << 2)
 #endif
+#ifndef IOCTX_FLAG_IOPOLL
+#define IOCTX_FLAG_IOPOLL	(1 << 0)
+#endif
 
 static int fio_libaio_commit(struct thread_data *td);
 
@@ -354,6 +357,25 @@ static void fio_libaio_cleanup(struct thread_data *td)
 	}
 }
 
+static int fio_libaio_queue_init(struct libaio_data *ld, unsigned int depth,
+				 bool hipri)
+{
+#ifdef __NR_sys_io_setup2
+	int err, flags = 0;
+
+	if (hipri)
+		flags = IOCTX_FLAG_IOPOLL;
+
+	return syscall(__NR_sys_io_setup2, depth, flags, &ld->aio_ctx);
+#else
+	if (hipri) {
+		log_err("fio: polled aio not available on your platform\n");
+		return 1;
+	}
+	return io_queue_init(depth, &ld->aio_ctx);
+#endif
+}
+
 static int fio_libaio_init(struct thread_data *td)
 {
 	struct libaio_options *o = td->eo;
@@ -368,9 +390,9 @@ static int fio_libaio_init(struct thread_data *td)
 	 * and we need the right depth.
 	 */
 	if (!o->userspace_reap)
-		err = io_queue_init(INT_MAX, &ld->aio_ctx);
+		err = fio_libaio_queue_init(ld, INT_MAX, o->hipri);
 	if (o->userspace_reap || err == -EINVAL)
-		err = io_queue_init(td->o.iodepth, &ld->aio_ctx);
+		err = fio_libaio_queue_init(ld, td->o.iodepth, o->hipri);
 	if (err) {
 		td_verror(td, -err, "io_queue_init");
 		log_err("fio: check /proc/sys/fs/aio-max-nr\n");

commit a1b006fe1cd3aa7e1b567f55e5a4c827d54f7c41

engines/libaio: fix new aio poll API
It'll be final. Some day.

Fixes: ebec344dd336 ("engines/libaio: update to new io_setup2() system call")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

arch/arch-x86_64.h
@@ -1,8 +1,8 @@
 #ifndef ARCH_X86_64_H
 #define ARCH_X86_64_H
 
-#ifndef __NR_sys_iosetup2
-#define __NR_sys_iosetup2	335
+#ifndef __NR_sys_io_setup2
+#define __NR_sys_io_setup2	335
 #endif
 
 static inline void do_cpuid(unsigned int *eax, unsigned int *ebx,

engines/libaio.c
@@ -361,7 +361,7 @@ static int fio_libaio_queue_init(struct libaio_data *ld, unsigned int depth,
 				 bool hipri)
 {
 #ifdef __NR_sys_io_setup2
-	int err, flags = 0;
+	int flags = 0;
 
 	if (hipri)
 		flags = IOCTX_FLAG_IOPOLL;
@@ -389,10 +389,7 @@ static int fio_libaio_init(struct thread_data *td)
 	 * care about the user ring. If that fails, the kernel is too old
 	 * and we need the right depth.
 	 */
-	if (!o->userspace_reap)
-		err = fio_libaio_queue_init(ld, INT_MAX, o->hipri);
-	if (o->userspace_reap || err == -EINVAL)
-		err = fio_libaio_queue_init(ld, td->o.iodepth, o->hipri);
+	err = fio_libaio_queue_init(ld, td->o.iodepth, o->hipri);
 	if (err) {
 		td_verror(td, -err, "io_queue_init");
 		log_err("fio: check /proc/sys/fs/aio-max-nr\n");