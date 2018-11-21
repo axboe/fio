axboe/fio

commit a1b006fe1cd3aa7e1b567f55e5a4c827d54f7c41

engines/libaio: fix new aio poll API
It'll be final. Some day.

Fixes: ebec344dd336 ("engines/libaio: update to new io_setup2() system call")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

engines/libaio.c
@@ -361,7 +361,7 @@ static int fio_libaio_queue_init(struct libaio_data *ld, unsigned int depth,
 				 bool hipri)
 {
 #ifdef __NR_sys_io_setup2
-	int err, flags = 0;
+	int flags = 0;
 
 	if (hipri)
 		flags = IOCTX_FLAG_IOPOLL;
@@ -389,10 +389,7 @@ static int fio_libaio_init(struct thread_data *td)
 	 * care about the user ring. If that fails, the kernel is too old
 	 * and we need the right depth.
 	 */
-	if (!o->userspace_reap)
-		err = fio_libaio_queue_init(ld, INT_MAX, o->hipri);
-	if (o->userspace_reap || err == -EINVAL)
-		err = fio_libaio_queue_init(ld, td->o.iodepth, o->hipri);
+	err = fio_libaio_queue_init(ld, td->o.iodepth, o->hipri);
 	if (err) {
 		td_verror(td, -err, "io_queue_init");
 		log_err("fio: check /proc/sys/fs/aio-max-nr\n");

arch/arch-x86_64.h
@@ -1,8 +1,8 @@
 #ifndef ARCH_X86_64_H
 #define ARCH_X86_64_H
 
-#ifndef __NR_sys_iosetup2
-#define __NR_sys_iosetup2	335
+#ifndef __NR_sys_io_setup2
+#define __NR_sys_io_setup2	335
 #endif
 
 static inline void do_cpuid(unsigned int *eax, unsigned int *ebx,

commit e65ef9593436f0f62df366f506f7c4d318b5cd71

engines/libaio: fallback to old io_setup() system call
We can't rely on the new one being there, if we fail calling
io_setup2(), fallback to io_setup() like before.

Fixes: a1b006fe1cd3 ("engines/libaio: fix new aio poll API")
Reported-by: Yi Zhang <yi.zhang@redhat.com>
Signed-off-by: Jens Axboe <axboe@kernel.dk>

engines/libaio.c
@@ -393,19 +393,9 @@ static void fio_libaio_cleanup(struct thread_data *td)
 	}
 }
 
-static int fio_libaio_queue_init(struct libaio_data *ld, unsigned int depth,
-				 bool hipri, bool useriocb)
+static int fio_libaio_old_queue_init(struct libaio_data *ld, unsigned int depth,
+				     bool hipri, bool useriocb)
 {
-#ifdef __NR_sys_io_setup2
-	int flags = 0;
-
-	if (hipri)
-		flags |= IOCTX_FLAG_IOPOLL;
-	if (useriocb)
-		flags |= IOCTX_FLAG_USERIOCB;
-
-	return syscall(__NR_sys_io_setup2, depth, flags, ld->user_iocbs, &ld->aio_ctx);
-#else
 	if (hipri) {
 		log_err("fio: polled aio not available on your platform\n");
 		return 1;
@@ -416,6 +406,27 @@ static int fio_libaio_queue_init(struct libaio_data *ld, unsigned int depth,
 	}
 
 	return io_queue_init(depth, &ld->aio_ctx);
+}
+
+static int fio_libaio_queue_init(struct libaio_data *ld, unsigned int depth,
+				 bool hipri, bool useriocb)
+{
+#ifdef __NR_sys_io_setup2
+	int ret, flags = 0;
+
+	if (hipri)
+		flags |= IOCTX_FLAG_IOPOLL;
+	if (useriocb)
+		flags |= IOCTX_FLAG_USERIOCB;
+
+	ret = syscall(__NR_sys_io_setup2, depth, flags, ld->user_iocbs,
+			&ld->aio_ctx);
+	if (!ret)
+		return 0;
+
+	return fio_libaio_old_queue_init(ld, depth, hipri, useriocb);
+#else
+	return fio_libaio_old_queue_init(ld, depth, hipri, useriocb);
 #endif
 }
 