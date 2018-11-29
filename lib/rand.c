axboe/fio

commit 811ac503a6196f13da22565eb165c278988da4da

Compression fixes
- memset() remainder of buffer instead of copying random segment.
  Provides more reliable compression (yes yes, you told me so).

- Allow 100 as well, just means full memset. Make that just set
  zero_buffers=1 instead, since we only need to do it once
  instead of for every write.

Signed-off-by: Jens Axboe <axboe@kernel.dk>

Signed-off-by: Jens Axboe <axboe@kernel.dk>

lib/rand.c
@@ -34,7 +34,6 @@
 */
 
 #include <string.h>
-#include <assert.h>
 #include "rand.h"
 #include "../hash.h"
 
@@ -95,10 +94,16 @@ unsigned long fill_random_buf_percentage(struct frand_state *fs, void *buf,
 					 unsigned int percentage,
 					 unsigned int segment, unsigned int len)
 {
-	unsigned int this_len, rep_len;
 	unsigned long r = __rand(fs);
+	unsigned int this_len;
 
-	assert(segment <= len);
+	if (percentage == 100) {
+		memset(buf, 0, len);
+		return 0;
+	}
+
+	if (segment > len)
+		segment = len;
 
 	if (sizeof(int) != sizeof(long *))
 		r *= (unsigned long) __rand(fs);
@@ -116,16 +121,10 @@ unsigned long fill_random_buf_percentage(struct frand_state *fs, void *buf,
 		len -= this_len;
 		buf += this_len;
 
-		/*
-		 * Now duplicate random chunk in rest of buf
-		 */
-		rep_len = segment - this_len;
-		if (rep_len > len)
-			rep_len = len;
+		if (this_len > len)
+			this_len = len;
 
-		memcpy(buf, buf + rep_len, rep_len);
-		buf += rep_len;
-		len -= rep_len;
+		memset(buf, 0, this_len);
 	}
 
 	return r;

init.c
@@ -568,6 +568,15 @@ static int fixup_options(struct thread_data *td)
 	}
 #endif
 
+	/*
+	 * For fully compressible data, just zero them at init time.
+	 * It's faster than repeatedly filling it.
+	 */
+	if (td->o.compress_percentage == 100) {
+		td->o.zero_buffers = 1;
+		td->o.compress_percentage = 0;
+	}
+
 	return ret;
 }
 

commit 3363fa4482e24fb87d8aea3443e11d90f1bfc462

rand: fix compressible data ratio per segment
I've noticed a bug in fio while testing it. I expected to receive
output similar to the expected output below for: "hexdump -n 4096
/dev/nvme0n1" (considering the configuration file I've used that may
be found below).

Expected output:

0000000 fdc6 a8a8 7190 0219 1fb8 9632 d9dd 1e64
/* random data */
00004c0 d8a3 13fe aeec 0fb6 5b14 162e 0000 0000
00004d0 0000 0000 0000 0000 0000 0000 0000 0000
*
0001000

However, the output I've received contained data after address 4cc
(which should have been filled with zeroes until 1000 as far as I
understand, but as you can see 99a contains data.

0000000 fdc6 a8a8 7190 0219 1fb8 9632 d9dd 1e64
/* random data */
00004c0 d8a3 13fe aeec 0fb6 5b14 162e 0000 0000
00004d0 0000 0000 0000 0000 0000 0000 0000 0000
*
0000990 0000 0000 0000 0000 fdc6 a8a8 7190 0219

Config file:

[global]
group_reporting=1
filename=/dev/nvme0n1
name=task_nvme0n1
rw=write
bs=4096
numjobs=1
iodepth=32
buffer_compress_chunk=4096
buffer_compress_percentage=70
cpus_allowed=0-8
cpus_allowed_policy=split
direct=1
ioengine=libaio
loops=1
refill_buffers=0

[job empty]
size=4096

Fio should write (100 - compress_percentage) * segemnt of random data
followed by compress_percentage * segemnt of compressible
data.

Currently, at each itereation fio fills (100 - compress_percentage) * segemnt
data, followed by (100 - compress_percentage) * segemnt of compressible
data until a the segment is filled.

Fixes: 811ac503a619 ("Compression fixes")
Signed-off-by: Bari Antebi <bari@lightbitslabs.com>
Signed-off-by: Jens Axboe <axboe@kernel.dk>

lib/rand.c
@@ -166,6 +166,7 @@ void __fill_random_buf_percentage(unsigned long seed, void *buf,
 		if (!len)
 			break;
 		buf += this_len;
+		this_len = segment - this_len;
 
 		if (this_len > len)
 			this_len = len;