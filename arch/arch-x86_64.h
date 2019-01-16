8287fee8a6f8ebb43081a3087d3a98bf6dc7f551
Jens Axboe <axboe@kernel.dk>
2019-01-15 21:43:11 -0700

    x86-64: correct read/write barriers
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- arch/arch-x86_64.h
+++ arch/arch-x86_64.h
@@ -27,8 +27,8 @@ static inline void do_cpuid(unsigned int *eax, unsigned int *ebx,
 #define	FIO_HUGE_PAGE		2097152
 
 #define nop		__asm__ __volatile__("rep;nop": : :"memory")
-#define read_barrier()	__asm__ __volatile__("lfence":::"memory")
-#define write_barrier()	__asm__ __volatile__("sfence":::"memory")
+#define read_barrier()	__asm__ __volatile__("":::"memory")
+#define write_barrier()	__asm__ __volatile__("":::"memory")
 
 static inline unsigned long arch_ffz(unsigned long bitmask)
 {
