839bea4f8e8f6cd00fb69028ae7a007643b53242
Jens Axboe <axboe@kernel.dk>
2019-04-25 10:09:27 -0600

    Fix __FIO_OPT_G_ISCSI numbering
    __FIO_OPT_G_NR must always be last, if it isn't then it doesn't
    denote the last used bit.
    
    Fixes: 247ef2aaf28a ("fio: add libiscsi engine")
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- optgroup.h
+++ optgroup.h
@@ -62,8 +62,8 @@ enum opt_category_group {
 	__FIO_OPT_G_HDFS,
 	__FIO_OPT_G_SG,
 	__FIO_OPT_G_MMAP,
-	__FIO_OPT_G_NR,
 	__FIO_OPT_G_ISCSI,
+	__FIO_OPT_G_NR,
 
 	FIO_OPT_G_RATE		= (1ULL << __FIO_OPT_G_RATE),
 	FIO_OPT_G_ZONE		= (1ULL << __FIO_OPT_G_ZONE),
