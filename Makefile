axboe/fio

commit 5ad80fa7cf5be0399c8d9d68967781e56a767c82

Makefile: Build more test code
Build the t/io_uring, t/memlock and t/read-to-pipe-async executables if
'make all' is executed. Do not include t/time-test because it uses a
non-portable 128-bit integer type.

Signed-off-by: Bart Van Assche <bvanassche@acm.org>

Makefile
@@ -313,6 +313,11 @@ T_TEST_PROGS += $(T_GEN_RAND_PROGS)
 T_PROGS += $(T_BTRACE_FIO_PROGS)
 T_PROGS += $(T_DEDUPE_PROGS)
 T_PROGS += $(T_VS_PROGS)
+T_TEST_PROGS += $(T_MEMLOCK_PROGS)
+T_TEST_PROGS += $(T_PIPE_ASYNC_PROGS)
+ifneq (,$(findstring Linux,$(CONFIG_TARGET_OS)))
+T_TEST_PROGS += $(T_IOU_RING_PROGS)
+endif
 
 PROGS += $(T_PROGS)
 

commit 15eca9aa125bf53932dc9872b0f733e97a500c3e

Only build t/read-to-pipe-async if pread() is available
Reported-by: Vincent Fu <vincent.fu@wdc.com>
Fixes: 5ad80fa7cf5b ("Makefile: Build more test code")
Signed-off-by: Bart Van Assche <bvanassche@acm.org>

Makefile
@@ -314,7 +314,9 @@ T_PROGS += $(T_BTRACE_FIO_PROGS)
 T_PROGS += $(T_DEDUPE_PROGS)
 T_PROGS += $(T_VS_PROGS)
 T_TEST_PROGS += $(T_MEMLOCK_PROGS)
+ifdef CONFIG_PREAD
 T_TEST_PROGS += $(T_PIPE_ASYNC_PROGS)
+endif
 ifneq (,$(findstring Linux,$(CONFIG_TARGET_OS)))
 T_TEST_PROGS += $(T_IOU_RING_PROGS)
 endif

configure
@@ -1111,6 +1111,23 @@ fi
 print_config "fdatasync" "$fdatasync"
 
 ##########################################
+# pread() probe
+if test "$pread" != "yes" ; then
+  pread="no"
+fi
+cat > $TMPC << EOF
+#include <unistd.h>
+int main(int argc, char **argv)
+{
+  return pread(0, NULL, 0, 0);
+}
+EOF
+if compile_prog "" "" "pread"; then
+  pread="yes"
+fi
+print_config "pread()" "$pread"
+
+##########################################
 # sync_file_range() probe
 if test "$sync_file_range" != "yes" ; then
   sync_file_range="no"
@@ -2492,6 +2509,9 @@ if test "$posix_fallocate" = "yes" ; then
 fi
 if test "$fdatasync" = "yes" ; then
   output_sym "CONFIG_FDATASYNC"
+fi
+if test "$pread" = "yes" ; then
+  output_sym "CONFIG_PREAD"
 fi
 if test "$sync_file_range" = "yes" ; then
   output_sym "CONFIG_SYNC_FILE_RANGE"