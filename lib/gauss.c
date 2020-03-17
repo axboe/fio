9853da1df43fb5b91097813651f69f50ca098a41
Feng Tang <feng.tang@intel.com>
2020-03-17 20:53:31 +0800

    gauss.c: correct the stddev initializtion
    Signed-off-by: Feng Tang <feng.tang@intel.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- lib/gauss.c
+++ lib/gauss.c
@@ -51,7 +51,7 @@ void gauss_init(struct gauss_state *gs, unsigned long nranges, double dev,
 	gs->nranges = nranges;
 
 	if (dev != 0.0) {
-		gs->stddev = ceil((double) (nranges * 100.0) / dev);
+		gs->stddev = ceil((double)(nranges * dev) / 100.0);
 		if (gs->stddev > nranges / 2)
 			gs->stddev = nranges / 2;
 	}
