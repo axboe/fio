a0ec1a1e6ecd39349325b26ef5300e291d18f821
Bart Van Assche <bvanassche@acm.org>
2020-05-24 14:40:42 -0700

    Switch to Python3
    Support for Python2.7 in Linux distributions is being phased out. As an
    example, some Linux distributions do not provide Python2.7 versions of
    some of the Python packages fio Python scripts relies on. Hence switch
    to Python3. Most of the Python code changes in this patch have been
    generated by running the Python scripts through 2to3, the Python2 to
    Python3 convertor from python.org.
    
    Notes:
    - Python is only used by fio test scripts and not by fio itself.
    - Older versions of RHEL / CentOS 6 do not support Python3.
      Python3 binaries for RHEL 6 / CentOS 6 are available e.g. in the EPEL
      repository (https://fedoraproject.org/wiki/EPEL).
    
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>

--- tools/fiologparser.py
+++ tools/fiologparser.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2.7
+#!/usr/bin/env python3
 # Note: this script is python2 and python 3 compatible.
 #
 # fiologparser.py
@@ -18,6 +18,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 import argparse
 import math
+from functools import reduce
 
 def parse_args():
     parser = argparse.ArgumentParser()
