a06e44772e4e1e5267949f1ab1558d7d56519e0f
Bart Van Assche <bvanassche@acm.org>
2020-06-07 12:11:01 -0700

    ci/travis-install.sh: Install python3-six package
    Additionally, report the Python3 path and version.
    
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- ci/travis-install.sh
+++ ci/travis-install.sh
@@ -2,6 +2,7 @@
 
 case "$TRAVIS_OS_NAME" in
     "linux")
+	# Architecture-dependent packages.
 	pkgs=(
 	    libaio-dev
 	    libcunit1
@@ -16,16 +17,27 @@ case "$TRAVIS_OS_NAME" in
 	)
 	if [[ "$BUILD_ARCH" == "x86" ]]; then
 	    pkgs=("${pkgs[@]/%/:i386}")
-	    pkgs+=(gcc-multilib python3-scipy)
+	    pkgs+=(gcc-multilib)
 	else
-	    pkgs+=(glusterfs-common python3-scipy)
+	    pkgs+=(glusterfs-common)
 	fi
+	# Architecture-independent packages and packages for which we don't
+	# care about the architecture.
+	pkgs+=(
+	    python3
+	    python3-scipy
+	    python3-six
+	)
 	sudo apt-get -qq update
 	sudo apt-get install --no-install-recommends -qq -y "${pkgs[@]}"
 	;;
     "osx")
-	brew update
+	brew update >/dev/null 2>&1
 	brew install cunit
 	pip3 install scipy
 	;;
 esac
+
+echo "Python version: $(/usr/bin/python -V 2>&1)"
+echo "Python3 path: $(which python3 2>&1)"
+echo "Python3 version: $(python3 -V 2>&1)"
