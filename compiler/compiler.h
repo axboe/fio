axboe/fio

commit baa16486051f83158ffca01fa2e76a7232bb16f3

compiler/compiler.h: minimum GCC version is 4.9
Signed-off-by: Jens Axboe <axboe@kernel.dk>

compiler/compiler.h
@@ -2,10 +2,10 @@
 #define FIO_COMPILER_H
 
 /* IWYU pragma: begin_exports */
-#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 1)
+#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 9)
 #include "compiler-gcc4.h"
 #else
-#error Compiler too old, need at least gcc 4.1.0
+#error Compiler too old, need at least gcc 4.9
 #endif
 /* IWYU pragma: end_exports */
 

commit 5b3da1650f5069547712d993b6096291d3b2bb14

compiler/compiler.h: include clang 6.0 and above
Fixes: baa16486051f ("compiler/compiler.h: minimum GCC version is 4.9")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

compiler/compiler.h
@@ -2,7 +2,7 @@
 #define FIO_COMPILER_H
 
 /* IWYU pragma: begin_exports */
-#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 9)
+#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 9) || __clang_major__ >= 6
 #include "compiler-gcc4.h"
 #else
 #error Compiler too old, need at least gcc 4.9