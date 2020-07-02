53916bbd100ef806a7af6ece50c4ee0569bac4c6
Bart Van Assche <bvanassche@acm.org>
2020-07-02 15:18:37 -0700

    Add a num2str() unit test
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>

--- unittests/unittest.c
+++ unittests/unittest.c
@@ -48,6 +48,7 @@ int main(void)
 	}
 
 	fio_unittest_register(fio_unittest_lib_memalign);
+	fio_unittest_register(fio_unittest_lib_num2str);
 	fio_unittest_register(fio_unittest_lib_strntol);
 	fio_unittest_register(fio_unittest_oslib_strlcat);
 	fio_unittest_register(fio_unittest_oslib_strndup);
