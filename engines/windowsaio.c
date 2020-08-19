axboe/fio

commit 09c1aa8b322765afae56ea1ebc9eaa06f94da6a6

engines/windowsaio: only set IOCP thread affinity if specified
We set it by default, which seems like a bad idea. If the issuing
thread is running on some other CPU, we don't want to bind this
to what is probably CPU0.

Signed-off-by: Jens Axboe <axboe@kernel.dk>

engines/windowsaio.c
@@ -107,7 +107,8 @@ static int fio_windowsaio_init(struct thread_data *td)
 			ctx->wd = wd;
 			wd->iothread = CreateThread(NULL, 0, IoCompletionRoutine, ctx, 0, &threadid);
 
-			if (wd->iothread != NULL)
+			if (wd->iothread != NULL &&
+			    fio_option_is_set(&td->o, cpumask))
 				fio_setaffinity(threadid, td->o.cpumask);
 			else
 				log_err("windowsaio: failed to create io completion thread\n");

commit 1dd90eec6cf5e68285dcab638b97a25f5f3018d4

engines/windowsaio: fix silly thinky on IO thread creation
I inadvertently made the setup fail unless the affinity option was
set, the intent was obviously to only run the affinitization if we
asked for it.

Fixes: 09c1aa8b3227 ("engines/windowsaio: only set IOCP thread affinity if specified")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

engines/windowsaio.c
@@ -106,12 +106,10 @@ static int fio_windowsaio_init(struct thread_data *td)
 			ctx->iocp = hFile;
 			ctx->wd = wd;
 			wd->iothread = CreateThread(NULL, 0, IoCompletionRoutine, ctx, 0, &threadid);
-
-			if (wd->iothread != NULL &&
-			    fio_option_is_set(&td->o, cpumask))
-				fio_setaffinity(threadid, td->o.cpumask);
-			else
+			if (!wd->iothread)
 				log_err("windowsaio: failed to create io completion thread\n");
+			else if (fio_option_is_set(&td->o, cpumask))
+				fio_setaffinity(threadid, td->o.cpumask);
 		}
 
 		if (rc || wd->iothread == NULL)