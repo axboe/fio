a9b9b1627d52137246eb3c76476f7645780a9beb
Sitsofe Wheeler <sitsofe@yahoo.com>
2020-09-03 23:02:31 +0100

    travis: cleanup build script
    - Standardise on spaces for indentation
    - Exit on error or using variables before they are defined
    - Fix up shellcheck complaints by exporting some variables
    - Drop printing of python 2 information as we don't use it
    - Use type -p rather than which
    
    Signed-off-by: Sitsofe Wheeler <sitsofe@yahoo.com>

--- ci/travis-build.sh
+++ ci/travis-build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+set -eu
 
 CI_TARGET_ARCH="${BUILD_ARCH:-$TRAVIS_CPU_ARCH}"
 EXTRA_CFLAGS="-Werror"
-PYTHONUNBUFFERED=TRUE
+export PYTHONUNBUFFERED=TRUE
 CONFIGURE_FLAGS=()
 
 case "$TRAVIS_OS_NAME" in
@@ -25,7 +26,7 @@ CONFIGURE_FLAGS+=(--extra-cflags="${EXTRA_CFLAGS}")
     make &&
     make test &&
     if [[ "$CI_TARGET_ARCH" == "arm64" ]]; then
-	sudo python3 t/run-fio-tests.py --skip 6 1007 1008 --debug -p 1010:"--skip 15 16 17 18 19 20"
+        sudo python3 t/run-fio-tests.py --skip 6 1007 1008 --debug -p 1010:"--skip 15 16 17 18 19 20"
     else
-	sudo python3 t/run-fio-tests.py --skip 6 1007 1008 --debug
+        sudo python3 t/run-fio-tests.py --skip 6 1007 1008 --debug
     fi
