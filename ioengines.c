axboe/fio

commit abfd235a05330eae8444f0e4c0039bbdf9f160a3

Disable io_submit_mode=offload with async engines
We have various cases that aren't handled correctly with async engines,
or cannot work with async engines. Disable it.

Signed-off-by: Jens Axboe <axboe@kernel.dk>

ioengines.c
@@ -22,7 +22,7 @@
 
 static FLIST_HEAD(engine_list);
 
-static bool check_engine_ops(struct ioengine_ops *ops)
+static bool check_engine_ops(struct thread_data *td, struct ioengine_ops *ops)
 {
 	if (ops->version != FIO_IOOPS_VERSION) {
 		log_err("bad ioops version %d (want %d)\n", ops->version,
@@ -40,6 +40,15 @@ static bool check_engine_ops(struct ioengine_ops *ops)
 	 */
 	if (ops->flags & FIO_SYNCIO)
 		return false;
+
+	/*
+	 * async engines aren't reliable with offload
+	 */
+	if (td->o.io_submit_mode == IO_MODE_OFFLOAD) {
+		log_err("%s: can't be used with offloaded submit. Use a sync "
+			"engine\n", ops->name);
+		return true;
+	}
 
 	if (!ops->event || !ops->getevents) {
 		log_err("%s: no event/getevents handler\n", ops->name);
@@ -193,7 +202,7 @@ struct ioengine_ops *load_ioengine(struct thread_data *td)
 	/*
 	 * Check that the required methods are there.
 	 */
-	if (check_engine_ops(ops))
+	if (check_engine_ops(td, ops))
 		return NULL;
 
 	return ops;

HOWTO
@@ -2474,7 +2474,8 @@ I/O depth
 	can increase latencies. The benefit is that fio can manage submission rates
 	independently of the device completion rates. This avoids skewed latency
 	reporting if I/O gets backed up on the device side (the coordinated omission
-	problem).
+	problem). Note that this option cannot reliably be used with async IO
+	engines.
 
 
 I/O rate

fio.1
@@ -2215,7 +2215,7 @@ has a bit of extra overhead, especially for lower queue depth I/O where it
 can increase latencies. The benefit is that fio can manage submission rates
 independently of the device completion rates. This avoids skewed latency
 reporting if I/O gets backed up on the device side (the coordinated omission
-problem).
+problem). Note that this option cannot reliably be used with async IO engines.
 .SS "I/O rate"
 .TP
 .BI thinktime \fR=\fPtime

commit 695611a9d4cd554d44d8b2ec5da2811061950a2e

Allow offload with FAKEIO engines
The last check was too restrictive, we can allow it with the fake IO
engines.

Fixes: abfd235a0533 ("Disable io_submit_mode=offload with async engines")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

ioengines.c
@@ -44,7 +44,8 @@ static bool check_engine_ops(struct thread_data *td, struct ioengine_ops *ops)
 	/*
 	 * async engines aren't reliable with offload
 	 */
-	if (td->o.io_submit_mode == IO_MODE_OFFLOAD) {
+	if ((td->o.io_submit_mode == IO_MODE_OFFLOAD) &&
+	    !(ops->flags & FIO_FAKEIO)) {
 		log_err("%s: can't be used with offloaded submit. Use a sync "
 			"engine\n", ops->name);
 		return true;