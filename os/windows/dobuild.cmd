ea693b6e4501a1385bf62a01f6fb1f3609d31a4a
Jens Axboe <axboe@kernel.dk>
2020-11-05 15:33:00 -0700

    Revert "Windows: update dobuild.cmd to run the configure/make"
    This reverts commit 38c2f9384db8dbd93f59d965d70ab0d3a53343fa.
    
    It's causing issues for CI, revert it for now.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- os/windows/dobuild.cmd
+++ os/windows/dobuild.cmd
@@ -1,18 +1,5 @@
 @echo off
 setlocal enabledelayedexpansion
-
-if "%1"=="x86" set FIO_ARCH=x86
-if "%1"=="x64" set FIO_ARCH=x64
-
-if not defined FIO_ARCH (
-  echo Error: must specify the architecture.
-  echo Usage: dobuild x86
-  echo Usage: dobuild x64
-  goto end
-)
-
-C:\Cygwin64\bin\bash -l %cd%/_domake.sh %FIO_ARCH% %cd%
-
 set /a counter=1
 for /f "tokens=3" %%i in (..\..\FIO-VERSION-FILE) do (
  if "!counter!"=="1" set FIO_VERSION=%%i
@@ -26,6 +13,16 @@ for /f "tokens=2 delims=-" %%i in ("%FIO_VERSION%") do (
 if not defined FIO_VERSION_NUMBERS (
   echo Could not find version numbers in the string '%FIO_VERSION%'
   echo Expected version to follow format 'fio-^([0-9]+.[0-9.]+^)'
+  goto end
+)
+
+if "%1"=="x86" set FIO_ARCH=x86
+if "%1"=="x64" set FIO_ARCH=x64
+
+if not defined FIO_ARCH (
+  echo Error: must specify the architecture.
+  echo Usage: dobuild x86
+  echo Usage: dobuild x64
   goto end
 )
 
