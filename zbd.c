axboe/fio

commit 000ecb5fe36dea8acbcbf8f185d00592222c95e1

zbd: Removing useless variable assignment
zone_idx_b is set but never read again.

Signed-off-by: Erwan Velu <erwanaliasr1@gmail.com>

zbd.c
@@ -1857,7 +1857,7 @@ enum io_u_action zbd_adjust_block(struct thread_data *td, struct io_u *io_u)
 				       f->file_name);
 				goto eof;
 			}
-			zone_idx_b = zbd_zone_nr(f, zb);
+			zbd_zone_nr(f, zb);
 		}
 		/* Check whether the zone reset threshold has been exceeded */
 		if (td->o.zrf.u.f) {

commit d5c3be105af97c71bc2095ffd19343e4217abcd7

zbd: remove dead zone retrieval call
A previous commit missed to realize that not only was the assignment
useless, that also made the very call to zbd_zone_nr() useless as
well. Remove it.

Fixes: 000ecb5fe36d ("zbd: Removing useless variable assignment")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

zbd.c
@@ -1857,7 +1857,6 @@ enum io_u_action zbd_adjust_block(struct thread_data *td, struct io_u *io_u)
 				       f->file_name);
 				goto eof;
 			}
-			zbd_zone_nr(f, zb);
 		}
 		/* Check whether the zone reset threshold has been exceeded */
 		if (td->o.zrf.u.f) {