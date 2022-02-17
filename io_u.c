axboe/fio

commit a04e0665cb5d3a545ab1dbe2d2b7c150b404735d

Use fcntl(..., F_FULLSYNC) if available
Some operating systems don't perform a data integrity flush when
fsync() is done, but provide fcntl(fd, F_FULLSYNC) to provide that kind
of guarantee.

To ensure that comparisons between operating systems is fair, use
fcntl() to do a proper sync if available.

Signed-off-by: Jens Axboe <axboe@kernel.dk>

configure
@@ -646,6 +646,25 @@ fi
 print_config "zlib" "$zlib"
 
 ##########################################
+# fcntl(F_FULLFSYNC) support
+if test "$fcntl_sync" != "yes" ; then
+  fcntl_sync="no"
+fi
+cat > $TMPC << EOF
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char **argv)
+{
+  return fcntl(0, F_FULLSYNC);
+}
+EOF
+if compile_prog "" "" "fcntl(F_FULLSYNC)" ; then
+    fcntl_sync="yes"
+fi
+print_config "fcntl(F_FULLSYNC)" "$fcntl_sync"
+
+##########################################
 # linux-aio probe
 if test "$libaio" != "yes" ; then
   libaio="no"
@@ -3173,6 +3192,9 @@ if test "$dynamic_engines" = "yes" ; then
 fi
 if test "$pdb" = yes; then
   output_sym "CONFIG_PDB"
+fi
+if test "$fcntl_sync" = "yes" ; then
+  output_sym "CONFIG_FCNTL_SYNC"
 fi
 
 print_config "Lib-based ioengines dynamic" "$dynamic_engines"

io_u.c
@@ -2297,7 +2297,11 @@ int do_io_u_sync(const struct thread_data *td, struct io_u *io_u)
 	int ret;
 
 	if (io_u->ddir == DDIR_SYNC) {
+#ifdef CONFIG_FCNTL_SYNC
+		ret = fcntl(io_u->file->fd, F_FULLSYNC);
+#else
 		ret = fsync(io_u->file->fd);
+#endif
 	} else if (io_u->ddir == DDIR_DATASYNC) {
 #ifdef CONFIG_FDATASYNC
 		ret = fdatasync(io_u->file->fd);

commit c99c81adb3510a8dc34d47fd40b19ef657e32192

Correct F_FULLSYNC -> F_FULLFSYNC
Apparently used a mix of the two, inconsistently.

Fixes: a04e0665cb5d ("Use fcntl(..., F_FULLSYNC) if available")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

configure
@@ -656,13 +656,13 @@ cat > $TMPC << EOF
 
 int main(int argc, char **argv)
 {
-  return fcntl(0, F_FULLSYNC);
+  return fcntl(0, F_FULLFSYNC);
 }
 EOF
-if compile_prog "" "" "fcntl(F_FULLSYNC)" ; then
+if compile_prog "" "" "fcntl(F_FULLFSYNC)" ; then
     fcntl_sync="yes"
 fi
-print_config "fcntl(F_FULLSYNC)" "$fcntl_sync"
+print_config "fcntl(F_FULLFSYNC)" "$fcntl_sync"
 
 ##########################################
 # linux-aio probe

io_u.c
@@ -2298,7 +2298,7 @@ int do_io_u_sync(const struct thread_data *td, struct io_u *io_u)
 
 	if (io_u->ddir == DDIR_SYNC) {
 #ifdef CONFIG_FCNTL_SYNC
-		ret = fcntl(io_u->file->fd, F_FULLSYNC);
+		ret = fcntl(io_u->file->fd, F_FULLFSYNC);
 #else
 		ret = fsync(io_u->file->fd);
 #endif