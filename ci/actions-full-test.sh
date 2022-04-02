7c799ab224ef4042967a9acca690cd33dbb34545
Denis Pronin <dannftk@yandex.ru>
2022-04-02 11:46:47 +0300

    actions-full-test.sh, removed sudo from the script
    the script might be called with 'sudo' from the call site, if required
    
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- ci/actions-full-test.sh
+++ ci/actions-full-test.sh
@@ -6,9 +6,9 @@ main() {
     echo "Running long running tests..."
     export PYTHONUNBUFFERED="TRUE"
     if [[ "${CI_TARGET_ARCH}" == "arm64" ]]; then
-        sudo python3 t/run-fio-tests.py --skip 6 1007 1008 --debug -p 1010:"--skip 15 16 17 18 19 20"
+        python3 t/run-fio-tests.py --skip 6 1007 1008 --debug -p 1010:"--skip 15 16 17 18 19 20"
     else
-        sudo python3 t/run-fio-tests.py --skip 6 1007 1008 --debug
+        python3 t/run-fio-tests.py --skip 6 1007 1008 --debug
     fi
     make -C doc html
 }
