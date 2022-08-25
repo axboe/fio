c27ae7ae6c3d9108bba80ff71cf36bf7fc8b34c9
Jens Axboe <axboe@kernel.dk>
2022-08-25 11:19:34 -0600

    engines/io_uring: delete debug code
    This was inadvertently introduced by a previous commit, get rid
    of it.
    
    Fixes: 1816895b788e ("engines/io_uring: pass back correct error value when interrupted")
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- engines/io_uring.c
+++ engines/io_uring.c
@@ -445,17 +445,11 @@ static struct io_u *fio_ioring_event(struct thread_data *td, int event)
 	struct io_uring_cqe *cqe;
 	struct io_u *io_u;
 	unsigned index;
-	static int eio;
 
 	index = (event + ld->cq_ring_off) & ld->cq_ring_mask;
 
 	cqe = &ld->cq_ring.cqes[index];
 	io_u = (struct io_u *) (uintptr_t) cqe->user_data;
-
-	if (eio++ == 5) {
-		printf("mark EIO\n");
-		cqe->res = -EIO;
-	}
 
 	if (cqe->res != io_u->xfer_buflen) {
 		if (cqe->res > io_u->xfer_buflen)
