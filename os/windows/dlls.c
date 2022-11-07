axboe/fio

commit 93bcfd20e37cef8cec350fe06d3a086724c9f257

Move Windows port to MinGW
- Add calls to WSAStartup in the network code as required by
  Winsock.
- Add Windows-specific init_random_state function which uses the
  Crypto API.
- Move Windows port to MinGW and update build system to create a
  64-bit binary by default.
- Install text files as .rtf so they won't open in Notepad by default
  (Wordpad understands Unix line endings; Notepad doesn't).
- Simplify WiX installer code.

Signed-off-by: Jens Axboe <axboe@kernel.dk>

os/windows/posix/include/syslog.h
@@ -1,0 +1,18 @@
+#ifndef SYSLOG_H
+#define SYSLOG_H
+
+int syslog();
+
+#define LOG_INFO 0
+#define LOG_ERROR 1
+#define LOG_WARN 2
+
+#define LOG_NDELAY 0
+#define LOG_NOWAIT 0
+#define LOG_PID 0
+#define LOG_USER 0
+
+void closelog(void);
+void openlog(const char *ident, int logopt, int facility);
+
+#endif /* SYSLOG_H */

HOWTO
@@ -627,6 +627,7 @@ iodepth_low=int	The low water mark indicating when to start filling
 
 direct=bool	If value is true, use non-buffered io. This is usually
 		O_DIRECT. Note that ZFS on Solaris doesn't support direct io.
+		On Windows the synchronous ioengines don't support direct io.
 
 buffered=bool	If value is true, use buffered io. This is the opposite
 		of the 'direct' option. Defaults to true.
@@ -644,7 +645,7 @@ fsync=int	If writing to a file, issue a sync of the dirty data
 
 fdatasync=int	Like fsync= but uses fdatasync() to only sync data and not
 		metadata blocks.
-		In FreeBSD there is no fdatasync(), this falls back to
+		In FreeBSD and Windows there is no fdatasync(), this falls back to
 		using fsync()
 
 sync_file_range=str:val	Use sync_file_range() for every 'val' number of

Makefile
@@ -9,7 +9,7 @@ PROGS	= fio
 SCRIPTS = fio_generate_plots
 UNAME  := $(shell uname)
 
-SOURCE = gettime.c fio.c ioengines.c init.c stat.c log.c time.c filesetup.c \
+SOURCE := gettime.c fio.c ioengines.c init.c stat.c log.c time.c filesetup.c \
 		eta.c verify.c memory.c io_u.c parse.c mutex.c options.c \
 		rbtree.c smalloc.c filehash.c profile.c debug.c lib/rand.c \
 		lib/num2str.c lib/ieee754.c $(wildcard crc/*.c) engines/cpu.c \
@@ -56,9 +56,11 @@ ifeq ($(UNAME), Darwin)
   LIBS	 += -lpthread -ldl
 endif
 ifneq (,$(findstring CYGWIN,$(UNAME)))
-  SOURCE += engines/windowsaio.c
-  LIBS	 += -lpthread -lrt -lpsapi
-  CFLAGS += -DPSAPI_VERSION=1
+  SOURCE := $(filter-out engines/mmap.c,$(SOURCE))
+  SOURCE += engines/windowsaio.c os/windows/posix.c
+  LIBS	 += -lpthread -lpsapi -lws2_32
+  CFLAGS += -DPSAPI_VERSION=1 -Ios/windows/posix/include -Wno-format
+  CC	  = x86_64-w64-mingw32-gcc
 endif
 
 OBJS = $(SOURCE:.c=.o)

README
@@ -109,21 +109,10 @@ based distros, it's typically called libaio-devel.
 Windows
 -------
 
-On Windows Cygwin (http://www.cygwin.com) is required with at least
-devel/gcc4 and devel/make installed in order to build fio, and
-admin/cygrunsrv to run it. You can also install devel/git to fetch/update
-the source files. To create an MSI installer package put a copy of Cygwin
-in os\windows\fio, install WiX 3.6 from http://wix.sourceforge.net/releases/
-and run dobuild.cmd from the os/windows directory.
-
-Before running fio you'll need to have a copy of cygserver running. Run 
-"/usr/bin/cygserver-config" from an elevated Cygwin shell (i.e. launch the
-Cygwin shell under the Administrator account) to configure it. Once
-configured, run "net start cygserver" to start it, or type
-"/usr/sbin/cygserver &" in the Cygwin shell to start a local copy. 
-
-If fio exits with the message "Bad system call" it normally means that
-Cygserver isn't running.
+On Windows MinGW (http://www.mingw.org/) is required in order to
+build fio. To create an MSI installer package install WiX 3.6 from
+http://wix.sourceforge.net/releases/ and run dobuild.cmd from the
+os/windows directory.
 
 
 Command line

client.c
@@ -404,6 +404,11 @@ int fio_clients_connect(void)
 	struct flist_head *entry, *tmp;
 	int ret;
 
+#ifdef WIN32
+	WSADATA wsd;
+	WSAStartup(MAKEWORD(2,2), &wsd);
+#endif
+
 	dprint(FD_NET, "client: connect all\n");
 
 	client_signal_handler();

diskutil.c
@@ -31,7 +31,7 @@ static void disk_util_free(struct disk_util *du)
 		flist_del(&slave->slavelist);
 		slave->users--;
 	}
-	
+
 	fio_mutex_remove(du->lock);
 	sfree(du);
 }

engines/mmap.c
@@ -59,7 +59,7 @@ static int fio_mmap_file(struct thread_data *td, struct fio_file *f,
 err:
 	if (td->error && f->mmap_ptr)
 		munmap(f->mmap_ptr, length);
-		
+
 	return td->error;
 }
 
@@ -200,7 +200,7 @@ static int fio_mmapio_init(struct thread_data *td)
 			break;
 		shift++;
 	} while (1);
-		
+
 	mmap_map_mask = 1UL << shift;
 	return 0;
 }

engines/windowsaio.c
@@ -1,6 +1,6 @@
 /*
  * Native Windows async IO engine
- * Copyright (C) 2011 Bruce Cran <bruce@cran.org.uk>
+ * Copyright (C) 2012 Bruce Cran <bruce@cran.org.uk>
  */
 
 #include <stdio.h>
@@ -113,9 +113,9 @@ static int fio_windowsaio_init(struct thread_data *td)
 	}
 
 	hKernel32Dll = GetModuleHandle("kernel32.dll");
-	wd->pCancelIoEx = GetProcAddress(hKernel32Dll, "CancelIoEx");
-
+	wd->pCancelIoEx = (CANCELIOEX)GetProcAddress(hKernel32Dll, "CancelIoEx");
 	td->io_ops->data = wd;
+
 	return rc;
 }
 
@@ -133,8 +133,9 @@ static void fio_windowsaio_cleanup(struct thread_data *td)
 		CloseHandle(wd->iothread);
 		CloseHandle(wd->iocomplete_event);
 
-		for (i = 0; i < td->o.iodepth; i++)
+		for (i = 0; i < td->o.iodepth; i++) {
 			CloseHandle(wd->ovls[i].o.hEvent);
+		}
 
 		free(wd->aio_events);
 		free(wd->ovls);
@@ -171,6 +172,10 @@ static int fio_windowsaio_open_file(struct thread_data *td, struct fio_file *f)
 	if (td->o.sync_io)
 		flags |= FILE_FLAG_WRITE_THROUGH;
 
+	/*
+	 * Inform Windows whether we're going to be doing sequential or
+	 * random io so it can tune the Cache Manager
+	 */
 	if (td->o.td_ddir == TD_DDIR_READ  ||
 		td->o.td_ddir == TD_DDIR_WRITE)
 		flags |= FILE_FLAG_SEQUENTIAL_SCAN;
@@ -182,7 +187,7 @@ static int fio_windowsaio_open_file(struct thread_data *td, struct fio_file *f)
 	else
 		access = (GENERIC_READ | GENERIC_WRITE);
 
-	if (td->o.create_on_open > 0)
+	if (td->o.create_on_open)
 		openmode = OPEN_ALWAYS;
 	else
 		openmode = OPEN_EXISTING;
@@ -193,7 +198,7 @@ static int fio_windowsaio_open_file(struct thread_data *td, struct fio_file *f)
 	if (f->hFile == INVALID_HANDLE_VALUE)
 		rc = 1;
 
-	/* Only set up the competion port and thread if we're not just
+	/* Only set up the completion port and thread if we're not just
 	 * querying the device size */
 	if (!rc && td->io_ops->data != NULL) {
 		struct thread_ctx *ctx;
@@ -308,7 +313,7 @@ static int fio_windowsaio_queue(struct thread_data *td, struct io_u *io_u)
 	LPOVERLAPPED lpOvl = NULL;
 	struct windowsaio_data *wd;
 	DWORD iobytes;
-	BOOL success;
+	BOOL success = FALSE;
 	int index;
 	int rc = FIO_Q_COMPLETED;
 
@@ -358,6 +363,7 @@ static int fio_windowsaio_queue(struct thread_data *td, struct io_u *io_u)
 		break;
 	default:
 		assert(0);
+		break;
 	}
 
 	if (success || GetLastError() == ERROR_IO_PENDING)

commit 4b387ecdec69f7b5be4d10b69cc0da6cd818665b

Windows: Fix the build
Fix the build errors about passing arguments without a prototype.

Fixes: 93bcfd20e37c ("Move Windows port to MinGW")
Signed-off-by: Bart Van Assche <bvanassche@acm.org>

os/windows/dlls.c
@@ -11,12 +11,18 @@ void os_clk_tck(long *clk_tck)
 	 */
 	unsigned long minRes, maxRes, curRes;
 	HMODULE lib;
-	FARPROC queryTimer;
-	FARPROC setTimer;
+	NTSTATUS NTAPI (*queryTimer)
+		(OUT PULONG              MinimumResolution,
+		 OUT PULONG              MaximumResolution,
+		 OUT PULONG              CurrentResolution);
+	NTSTATUS NTAPI (*setTimer)
+		(IN ULONG                DesiredResolution,
+		 IN BOOLEAN              SetResolution,
+		 OUT PULONG              CurrentResolution);
 
 	if (!(lib = LoadLibrary(TEXT("ntdll.dll"))) ||
-		!(queryTimer = GetProcAddress(lib, "NtQueryTimerResolution")) ||
-		!(setTimer = GetProcAddress(lib, "NtSetTimerResolution"))) {
+		!(queryTimer = (void *)GetProcAddress(lib, "NtQueryTimerResolution")) ||
+		!(setTimer = (void *)GetProcAddress(lib, "NtSetTimerResolution"))) {
 		dprint(FD_HELPERTHREAD, 
 			"Failed to load ntdll library, set to lower bound 64 Hz\n");
 		*clk_tck = 64;

os/windows/posix/include/syslog.h
@@ -1,7 +1,7 @@
 #ifndef SYSLOG_H
 #define SYSLOG_H
 
-int syslog();
+int syslog(int priority, const char *format, ...);
 
 #define LOG_INFO	0x1
 #define LOG_ERROR	0x2