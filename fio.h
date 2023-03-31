cfc4ad2ec90e98cc92f8ec911f6ae04e11cf2ba4
suho.son <suho.son@samsung.com>
2023-03-31 16:10:09 +0900

    thinktime: Fix missing re-init thinktime when using ramptime
    Prevent I/O bursts after ramptime due to thinktime.
    
    Each thread generates a certain amount of I/O requests,
    configured by thinktime_blocks.
    When ramptime ends, thinktime_blocks can't control I/O.
    Because thinktime_blocks are not reinitialized after ramptime.
    I fixed it by reinitializing last_thinktime and last_thinktime_blocks
    when ramptime ended.
    
    Signed-off by: Suho Son <suho.son@samsung.com>

--- fio.h
+++ fio.h
@@ -376,7 +376,7 @@ struct thread_data {
 
 	uint64_t *thinktime_blocks_counter;
 	struct timespec last_thinktime;
-	uint64_t last_thinktime_blocks;
+	int64_t last_thinktime_blocks;
 
 	/*
 	 * State for random io, a bitmap of blocks done vs not done
