673159e61b745adbee5135b710e59d7cef54e9d3
celestinechen <166680286+celestinechen@users.noreply.github.com>
2024-04-19 06:22:15 +0000

    FIO with fsync option issues more DDIR_SYNC commands than expected
    Issue and root cause:
    When fsync option is used, the number of flush (or DDIR_SYNC) commands
    issued is more than the expected number of flush commands.
    To elaborate:
    - In the fio config file, consider fsync=1
    1. FIO issues 1 write command
    2. After write completes, FIO sets last_was_sync variable to false
    3. FIO issues 1 flush command
    4. FIO keeps issuing flush commands since last_was_sync is still false
    and this causes more flush commands to be issued than expected
    5. last_was_sync is set to true after the flush command completes
    - The above steps repeats until the workload is completed.
    Fix:
    Instead of setting last_was_sync to true after flush command is completed
    and setting last_was_sync to false after write command is completed,
    set last_was_sync to true after flush command is issued and set
    last_was_sync to false after write command is issued.
    
    Signed-off-by: Celestine Chen celestinechen@google.com

--- io_u.c
+++ io_u.c
@@ -2113,7 +2113,6 @@ static void io_completed(struct thread_data *td, struct io_u **io_u_ptr,
 	if (ddir_sync(ddir)) {
 		if (io_u->error)
 			goto error;
-		td->last_was_sync = true;
 		if (f) {
 			f->first_write = -1ULL;
 			f->last_write = -1ULL;
@@ -2123,7 +2122,6 @@ static void io_completed(struct thread_data *td, struct io_u **io_u_ptr,
 		return;
 	}
 
-	td->last_was_sync = false;
 	td->last_ddir = ddir;
 
 	if (!io_u->error && ddir_rw(ddir)) {
