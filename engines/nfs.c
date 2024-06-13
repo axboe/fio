7d993b1011529d2037a02f6192e5dce155ebcd8d
Pan Xiao <pan.xiao@dell.com>
2024-06-13 09:09:00 +0800

    For nfs engine, treat randwrite the same as write when opening file
    When use nfs engine, only write mode can work if file doesn't exist,
    otherwise user has to make sure file exists before test starts.
    This change will support randwrite mode when file doesn't exist.
    
    Pan Xiao <xiaopan@outlook.com>

--- engines/nfs.c
+++ engines/nfs.c
@@ -280,7 +280,7 @@ static int fio_libnfs_open(struct thread_data *td, struct fio_file *f)
 	nfs_data = calloc(1, sizeof(struct nfs_data));
 	nfs_data->options = options;
 
-	if (td->o.td_ddir == TD_DDIR_WRITE)
+	if (td->o.td_ddir == TD_DDIR_WRITE || td->o.td_ddir == TD_DDIR_RANDWRITE)
 		flags |= O_CREAT | O_RDWR;
 	else
 		flags |= O_RDWR;
