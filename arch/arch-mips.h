341ed02e0d632a0409cc9b1c5eb426564f1b0ae7
Rosen Penev <rosenp@gmail.com>
2024-06-28 13:45:32 -0700

    fio: fix formats under MIPS64/PPC
    __SANE_USERSPACE_TYPES__ needs to be defined to get consistent formats on all platforms.
    
    It mostly affects 64-bit architectures (no op on 32 bit) with long long
    vs long.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- arch/arch-mips.h
+++ arch/arch-mips.h
@@ -3,6 +3,10 @@
 
 #define FIO_ARCH	(arch_mips)
 
+#ifndef __SANE_USERSPACE_TYPES__
+#define __SANE_USERSPACE_TYPES__
+#endif
+
 #define read_barrier()		__asm__ __volatile__("": : :"memory")
 #define write_barrier()		__asm__ __volatile__("": : :"memory")
 #define nop			__asm__ __volatile__("": : :"memory")
