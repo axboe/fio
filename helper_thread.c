409ae9e40bc534e2dd43a04c2b8154b48fdce898
Richard Barnes <rbarnes@umn.edu>
2024-07-19 10:46:10 -0700

    Update helper_thread.c

--- helper_thread.c
+++ helper_thread.c
@@ -110,7 +110,7 @@ static void block_signals(void)
 	sigset_t sigmask;
 
 	int ret;
-	(void)ret; // Suppress unused variable warning
+	(void)ret; /* Suppress unused variable warning */
 
 	ret = pthread_sigmask(SIG_UNBLOCK, NULL, &sigmask);
 	assert(ret == 0);
