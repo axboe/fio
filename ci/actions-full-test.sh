3671e62bc7caab2069a957af1342e027bc90303b
Vincent Fu <vincent.fu@samsung.com>
2024-08-19 20:34:33 +0000

    ci: remove arm64 case for tests
    We no longer run tests on arm64 platforms, so remove related part of the
    shell script.
    
    Signed-off-by: Vincent Fu <vincent.fu@samsung.com>

--- ci/actions-full-test.sh
+++ ci/actions-full-test.sh
@@ -10,11 +10,7 @@ main() {
 
     echo "Running long running tests..."
     export PYTHONUNBUFFERED="TRUE"
-    if [[ "${CI_TARGET_ARCH}" == "arm64" ]]; then
-        python3 t/run-fio-tests.py --skip 6 1007 1008 --debug -p 1010:"--skip 15 16 17 18 19 20"
-    else
-        python3 t/run-fio-tests.py --skip 6 1007 1008 --debug
-    fi
+    python3 t/run-fio-tests.py --skip 6 1007 1008 --debug
     make -C doc html
 }
 
