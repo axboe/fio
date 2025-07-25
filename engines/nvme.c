2fb6a2782ab1a21751fade28e11c0ed4fb528e19
Vincent Fu <vincentfu@gmail.com>
2025-07-25 13:57:59 -0400

    engines/nvme: refactor filling protection information
    Factor out of fio_nvme_pi_fill() the code that generates and fills in
    the Guard Protection Information field. This is so that later patches
    can use this code without filling in the fields in the NVMe command.
    
    No functional change intended.
    
    Signed-off-by: Anuj Gupta <anuj20.g@samsung.com>
    Signed-off-by: Vincent Fu <vincent.fu@samsung.com>
    Link: https://lore.kernel.org/r/20250725175808.2632-4-vincent.fu@samsung.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- engines/nvme.c
+++ engines/nvme.c
@@ -405,6 +405,18 @@ int fio_nvme_uring_cmd_prep(struct nvme_uring_cmd *cmd, struct io_u *io_u,
 	return 0;
 }
 
+void fio_nvme_generate_guard(struct io_u *io_u, struct nvme_cmd_ext_io_opts *opts)
+{
+	struct nvme_data *data = FILE_ENG_DATA(io_u->file);
+
+	if (data->pi_type && !(opts->io_flags & NVME_IO_PRINFO_PRACT)) {
+		if (data->guard_type == NVME_NVM_NS_16B_GUARD)
+			fio_nvme_generate_pi_16b_guard(data, io_u, opts);
+		else if (data->guard_type == NVME_NVM_NS_64B_GUARD)
+			fio_nvme_generate_pi_64b_guard(data, io_u, opts);
+	}
+}
+
 void fio_nvme_pi_fill(struct nvme_uring_cmd *cmd, struct io_u *io_u,
 		      struct nvme_cmd_ext_io_opts *opts)
 {
@@ -414,12 +426,7 @@ void fio_nvme_pi_fill(struct nvme_uring_cmd *cmd, struct io_u *io_u,
 	slba = get_slba(data, io_u->offset);
 	cmd->cdw12 |= opts->io_flags;
 
-	if (data->pi_type && !(opts->io_flags & NVME_IO_PRINFO_PRACT)) {
-		if (data->guard_type == NVME_NVM_NS_16B_GUARD)
-			fio_nvme_generate_pi_16b_guard(data, io_u, opts);
-		else if (data->guard_type == NVME_NVM_NS_64B_GUARD)
-			fio_nvme_generate_pi_64b_guard(data, io_u, opts);
-	}
+	fio_nvme_generate_guard(io_u, opts);
 
 	switch (data->pi_type) {
 	case NVME_NS_DPS_PI_TYPE1:
--- engines/nvme.h
+++ engines/nvme.h
@@ -438,6 +438,8 @@ int fio_nvme_uring_cmd_prep(struct nvme_uring_cmd *cmd, struct io_u *io_u,
 void fio_nvme_pi_fill(struct nvme_uring_cmd *cmd, struct io_u *io_u,
 		      struct nvme_cmd_ext_io_opts *opts);
 
+void fio_nvme_generate_guard(struct io_u *io_u, struct nvme_cmd_ext_io_opts *opts);
+
 int fio_nvme_pi_verify(struct nvme_data *data, struct io_u *io_u);
 
 int fio_nvme_get_zoned_model(struct thread_data *td, struct fio_file *f,
