axboe/fio

commit 8c8e7050ccd96a9afa3f92b7b3ab03d1c7ef8fc1

sprandom: drop validity_dist after use
The validity_dist buffer is only needed to compute the invalid_pct
array. Once that is done, we can free it instead of keeping it
around unnecessarily.

Signed-off-by: Tomas Winkler <tomas.winkler@sandisk.com>

sprandom.c
@@ -590,9 +590,6 @@ static int sprandom_setup(struct sprandom_info *spr_info, uint64_t logical_size,
 	/* Initialize validity_distribution */
 	print_d_array("validity resampled:", validity_dist, spr_info->num_regions);
 
-	spr_info->validity_dist = validity_dist;
-	total_alloc += spr_info->num_regions * sizeof(spr_info->validity_dist[0]);
-
 	/* Precompute invalidity percentage array */
 	spr_info->invalid_pct = calloc(spr_info->num_regions,
 				       sizeof(spr_info->invalid_pct[0]));
@@ -605,6 +602,8 @@ static int sprandom_setup(struct sprandom_info *spr_info, uint64_t logical_size,
 		double inv = (1.0 - validity_dist[i]) * (double)PCT_PRECISION;
 		spr_info->invalid_pct[i] = (int)round(inv);
 	}
+	free(validity_dist);
+	validity_dist = NULL;
 
 	region_sz = physical_size / spr_info->num_regions;
 	region_write_count = region_sz / align_bs;
@@ -644,7 +643,7 @@ static int sprandom_setup(struct sprandom_info *spr_info, uint64_t logical_size,
 
 	return 0;
 err:
-	free(spr_info->validity_dist);
+	free(validity_dist);
 	free(spr_info->invalid_pct);
 	return -ENOMEM;
 }
@@ -830,7 +829,6 @@ void sprandom_free(struct sprandom_info *info)
 	if (!info)
 		return;
 
-	free(info->validity_dist);
 	free(info->invalid_pct);
 	free(info->invalid_buf);
 	free(info);

sprandom.h
@@ -33,12 +33,11 @@ struct sprandom_info {
 	uint64_t  region_sz;
 	uint32_t  num_regions;
 
-	double    *validity_dist;
 	uint32_t  *invalid_pct;
 
 	/* Invalidation list*/
 	struct pc_buf *invalid_buf;
-	uint64_t  invalid_capacity;
+	uint64_t invalid_capacity;
 	size_t   invalid_count[2];
 	uint32_t current_region;
 	uint32_t curr_phase;

commit b7ce90b60a28386624bb9be36b4242887f1ad693

sprandom: wait to free validity_dist
This array is actually used to calculate invalid_capacity. So wait to
free it until the very end.

Fixes: 8c8e7050ccd9 ("sprandom: drop validity_dist after use")
Signed-off-by: Vincent Fu <vincent.fu@samsung.com>

sprandom.c
@@ -602,8 +602,6 @@ static int sprandom_setup(struct sprandom_info *spr_info, uint64_t logical_size,
 		double inv = (1.0 - validity_dist[i]) * (double)PCT_PRECISION;
 		spr_info->invalid_pct[i] = (int)round(inv);
 	}
-	free(validity_dist);
-	validity_dist = NULL;
 
 	region_sz = physical_size / spr_info->num_regions;
 	region_write_count = region_sz / align_bs;
@@ -641,6 +639,7 @@ static int sprandom_setup(struct sprandom_info *spr_info, uint64_t logical_size,
 		total_alloc,
 		bytes2str_simple(bytes2str_buf, sizeof(bytes2str_buf), total_alloc));
 
+	free(validity_dist);
 	return 0;
 err:
 	free(validity_dist);