axboe/fio

commit acce4fcaac6218eaa9100dec858507dc712dd3cb

memory: add cuda 13 support
In the latest cuda driver API (13.0), cuCtxCreate takes another
parameter CUctxCreateParams.

Fixes: #1988
Signed-off-by: Maxime Peim <maxime.peim@gmail.com>

memory.c
@@ -215,6 +215,9 @@ static int alloc_mem_cudamalloc(struct thread_data *td, size_t total_mem)
 #ifdef CONFIG_CUDA
 	CUresult ret;
 	char name[128];
+#ifdef CONFIG_CUDA13
+	CUctxCreateParams ctx_params = {};
+#endif
 
 	ret = cuInit(0);
 	if (ret != CUDA_SUCCESS) {
@@ -250,7 +253,11 @@ static int alloc_mem_cudamalloc(struct thread_data *td, size_t total_mem)
 	dprint(FD_MEM, "dev_id = [%d], device name = [%s]\n", \
 	       td->gpu_dev_id, name);
 
-	ret = cuCtxCreate(&td->cu_ctx, CU_CTX_MAP_HOST, td->cu_dev);
+#ifdef CONFIG_CUDA13
+	ret = cuCtxCreate(&td->cu_ctx, &ctx_params, CU_CTX_MAP_HOST, td->cu_dev);
+#else
+	ret = cuCtxCreate(&td->cu_ctx CU_CTX_MAP_HOST, td->cu_dev);
+#endif
 	if (ret != CUDA_SUCCESS) {
 		log_err("fio: failed to create cuda context: %d\n", ret);
 		return 1;

configure
@@ -180,6 +180,7 @@ libhdfs="no"
 devdax="no"
 pmem="no"
 cuda="no"
+cuda13="no"
 libcufile="no"
 disable_lex=""
 disable_pmem="no"
@@ -2811,6 +2812,26 @@ fi
 print_config "libcufile" "$libcufile"
 
 ##########################################
+# cuda 13 probe
+if test "$cuda" != "no" || test "$libcufile" != "no"; then
+cat > $TMPC << EOF
+#include <cuda.h>
+
+int main(int argc, char **argv)
+{
+  cuCtxCreate(NULL, NULL, 0, NULL);
+  return 0;
+}
+EOF
+  if compile_prog "" "-lcuda" "cuda13"; then
+    cuda13="yes"
+  else
+    cuda13="no"
+  fi
+  print_config "cuda>=13" "$cuda13"
+fi
+
+##########################################
 # check for cc -march=native
 build_native="no"
 cat > $TMPC << EOF
@@ -3304,6 +3325,9 @@ if test "$cuda" = "yes" ; then
 fi
 if test "$libcufile" = "yes" ; then
   output_sym "CONFIG_LIBCUFILE"
+fi
+if test "$cuda13" = "yes" ; then
+  output_sym "CONFIG_CUDA13"
 fi
 if test "$dfs" = "yes" ; then
   output_sym "CONFIG_DFS"

commit ae3475defe994e855f912d4b8dadf69c479b9ca2

memory: fix typo in cuda < 13 cuCtxCreate()
A previous commit added the 4 argument variant, but then also messed
up the previous one at the same time. Fix it up.

Link: https://github.com/axboe/fio/pull/2000
Fixes: acce4fcaac62 ("memory: add cuda 13 support")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

memory.c
@@ -256,7 +256,7 @@ static int alloc_mem_cudamalloc(struct thread_data *td, size_t total_mem)
 #ifdef CONFIG_CUDA13
 	ret = cuCtxCreate(&td->cu_ctx, &ctx_params, CU_CTX_MAP_HOST, td->cu_dev);
 #else
-	ret = cuCtxCreate(&td->cu_ctx CU_CTX_MAP_HOST, td->cu_dev);
+	ret = cuCtxCreate(&td->cu_ctx, CU_CTX_MAP_HOST, td->cu_dev);
 #endif
 	if (ret != CUDA_SUCCESS) {
 		log_err("fio: failed to create cuda context: %d\n", ret);