axboe/fio

commit 1c83ed15d57b324ddafdcd97e2c02c4ea2da20f5

io_u: get io_u from io_u_freelist when TD_FSYNCING
As Commit 813445e71292 ('backend: clean up requeued io_u's') has been
applied, backend cleans up the remained io_u's in td->io_u_requeues.
However, with end_fsync=1, the __get_io_u() function returns an io_u
from td->io_u_requeues if any io_u exist, and pops it. This leads that
the synced io_u will not put file which it got, and, finally, cannot
close the file.

This patch returns io_u from td->io_u_free_list when td->runstate is
TD_FSYNCING, so that the io_u's in td->io_u_requeues will be cleaned up
and leads to close file appropriately.

Signed-off-by: Jonghwi Jeong <jongh2.jeong@samsung.com>

io_u.c
@@ -1701,7 +1701,7 @@ struct io_u *__get_io_u(struct thread_data *td)
 		__td_io_u_lock(td);
 
 again:
-	if (!io_u_rempty(&td->io_u_requeues)) {
+	if (td->runstate != TD_FSYNCING && !io_u_rempty(&td->io_u_requeues)) {
 		io_u = io_u_rpop(&td->io_u_requeues);
 		io_u->resid = 0;
 	} else if (!queue_full(td)) {

commit be75cf8a10daa922afdef4b7a5edb9cf4d0175b8

io_u: put io_u->file from requeue in FSYNCING
When iodepth=1 and ->queue() returns FIO_Q_BUSY, the only io_u is pushed
to io_u_requeues. With end_fsync=1, the backend cannot retrieve io_u
from either io_u_freelist or io_u_requeues, leading to end_fsync
failure.

This patch fixes the issue by calling put_file_log() in __get_io_u() if
the runstate is TD_FSYNCING and io_u->file is not NULL. This ensures the
file reference is properly released, allowing end_fsync to succeed.

Fixes: 1c83ed1 ("io_u: get io_u from io_u_freelist when TD_FSYNCING")
Signed-off-by: Jonghwi Jeong <jongh2.jeong@samsung.com>

io_u.c
@@ -1727,9 +1727,13 @@ struct io_u *__get_io_u(struct thread_data *td)
 		__td_io_u_lock(td);
 
 again:
-	if (td->runstate != TD_FSYNCING && !io_u_rempty(&td->io_u_requeues)) {
+	if (!io_u_rempty(&td->io_u_requeues)) {
 		io_u = io_u_rpop(&td->io_u_requeues);
 		io_u->resid = 0;
+		if (io_u->file && td->runstate == TD_FSYNCING) {
+			put_file_log(td, io_u->file);
+			io_u->file = NULL;
+		}
 	} else if (!queue_full(td)) {
 		io_u = io_u_qpop(&td->io_u_freelist);
 